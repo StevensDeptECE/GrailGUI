ed6c46ff7b0a3b6bb0177836f9c0d1f2ed2ae39a
Alice Huston <aliceghuston@gmail.com>
2021-05-28 15:47:25 -0400

    Error handling, Buffer i/o, .gitignore
    Added additional error handling for socket i/o. Fixed a Buffer i/o bug
    that prevented Windows Buffers from writing to files without crashing.
    Modified the .vscode line in .gitignore, as a comment was allowing
    config files to be tracked.

--- src/csp/SocketIO.cc
+++ src/csp/SocketIO.cc
@@ -1,11 +1,22 @@
 #include "csp/SocketIO.hh"
+#include "csp/csp.hh"
 
 #include <cstdint>
 
+
 int SocketIO::send(socket_t sckt, const char *buf, int size, int flags){
-  return ::send(sckt, (char *)buf, size, 0);
+	uint32_t bytesSent;
+	if((bytesSent = ::send(sckt, (char *)buf, size, 0)) == err_code){
+		std::cout << "Error: " << WSAGetLastError() << std::endl;
+		throw Ex1(Errcode::SOCKET_SEND);
+	}
+	return bytesSent;
 }
 
 int SocketIO::recv(socket_t sckt, const char *buf, int size, int flags){
-  return ::recv(sckt, (char *)buf, size, 0);
+	uint32_t bytesRecv;
+	if((bytesRecv = ::recv(sckt, (char *)buf, size, 0)) == err_code){
+		throw Ex1(Errcode::SOCKET_RECV);
+	}
+	return bytesRecv;
 }
