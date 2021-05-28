2da70b7120dee2a10c3838df45cf028255fe7a42
ahuston-0 <aliceghuston@gmail.com>
2021-05-28 16:13:04 -0400

    Fixed a Linux compatibility bug

--- src/csp/SocketIO.cc
+++ src/csp/SocketIO.cc
@@ -3,11 +3,11 @@
 
 #include <cstdint>
 
-
+// TODO: Look into logging WSAGetLastError and strerror(errno)
+// Using the logging object in csp.hh?
 int SocketIO::send(socket_t sckt, const char *buf, int size, int flags){
 	uint32_t bytesSent;
 	if((bytesSent = ::send(sckt, (char *)buf, size, 0)) == err_code){
-		std::cout << "Error: " << WSAGetLastError() << std::endl;
 		throw Ex1(Errcode::SOCKET_SEND);
 	}
 	return bytesSent;
