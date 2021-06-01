d949597e90ca74b94ec43b5ef94751a4a56162a9
Quentin Jimenez <qjimenez@stevens.edu>
2021-06-01 11:03:58 -0400

    Workin Button Widget

--- test/testWidgets.cc
+++ test/testWidgets.cc
@@ -1,31 +1,47 @@
 #include "opengl/GrailGUI.hh"
 #include "opengl/ButtonWidget.hh"
 #include "opengl/GraphWidget.hh"
+#include "opengl/MultiText.hh"
 using namespace std;
+using namespace grail;
 
 class TestWidgets : public GLWin {
  public:
+ TestWidgets() :
+ 
+		GLWin(0x000000, 0xCCCCCC, "Window") {}
   void init() {
+    
     const Style* s = new Style("TIMES", 24, 1,
+          0,0,0,
+          0,1,0);
+
+    const Style* s2 = new Style("TIMES", 24, 1,
           0,1,0,0,
-          0,0,0,0);
+          0,0,0,0);          
+
     MainCanvas* c = currentTab()->getMainCanvas();
     StyledMultiShape2D* gui = c->getGui();
-    MultiText* guiText = c->getGuiText();
-    ButtonWidget b(gui, guiText, "hello", 0, 0, 100, 50);
-    b.init();
 
-    GraphWidget graph(gui, guiText, 0, 300, 400, 200);
-    graph.setTitleStyle(s);
-    graph.setLineStyle(s);
-    //graph.setXAxisStyle(s);
-    //graph.setYAxisStyle(s);
-    //graph.setTitle("sin(x)");
-    //graph.setXAxis("x");
-    //graph.setYAxis("x");
+    //MultiText* guiText = c->getGuiText();
+    MultiText* guiText = c->addLayer(new MultiText(c,s));
+
+    const float boxSize = 100;
+		const float drawSize = (boxSize/4)*5;
+
+    gui->fillRectangle(boxSize, boxSize, drawSize, drawSize/5, black);
+    gui->fillRectangle(boxSize, boxSize, drawSize/5, drawSize, black);
+    gui->fillRectangle(boxSize, boxSize*2, drawSize, drawSize/5, black);
+    gui->fillRectangle(boxSize*2, boxSize*1.5, drawSize/5, drawSize*.4, black);
+    gui->fillRectangle(boxSize*1.5, boxSize*1.5, drawSize*.4, drawSize/5, black);
+
+    
     float x[] = {100, 200, 300};
     float y[] = {50, 350, 222};
-    //graph.lineGraph(x, y);
+
+    //c->addButton("Hello",100,100,100,100);
+    ButtonWidget b(gui, guiText, "hello", 0, 0, 100, 50);
+    b.init();
 
   }
 };
