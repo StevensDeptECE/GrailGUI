9d77cb7d60e5c6eec178c7f577550124f8b85d4f
Alice Huston <aliceghuston@gmail.com>
2021-06-04 18:53:44 -0400

    Formatting, ESRI Shapefiles, libshape
    Fixed a workspace setting for VS Code that hopefully fixes formatting
    issues others were having. Added the libshape library to process ESRI
    Shapefiles in Grail. This could be replaced by a homebrewed version later,
    if necessary. We also have to determine how to include it in the build,
    as there is an outdated version available in the MSYS repositories.
    Lastly, bindings for libshape were added so we can read and store
    shape data to be drawn with Grail.

--- src/csp/SocketIO.cc
+++ src/csp/SocketIO.cc
@@ -1,22 +1,25 @@
 #include "csp/SocketIO.hh"
-#include "csp/csp.hh"
 
 #include <cstdint>
 
+#include "csp/csp.hh"
+
 // TODO: Look into logging WSAGetLastError and strerror(errno)
 // Using the logging object in csp.hh?
-int SocketIO::send(socket_t sckt, const char *buf, int size, int flags){
-	uint32_t bytesSent;
-	if((bytesSent = ::send(sckt, (char *)buf, size, 0)) == err_code){
-		throw Ex1(Errcode::SOCKET_SEND);
-	}
-	return bytesSent;
+int SocketIO::send(socket_t sckt, const char *buf, int size, int flags) {
+  uint32_t bytesSent;
+  if ((bytesSent = ::send(sckt, (char *)buf, size, 0)) == err_code) {
+    std::cout << WSAGetLastError() << std::endl;
+    throw Ex1(Errcode::SOCKET_SEND);
+  }
+  return bytesSent;
 }
 
-int SocketIO::recv(socket_t sckt, const char *buf, int size, int flags){
-	uint32_t bytesRecv;
-	if((bytesRecv = ::recv(sckt, (char *)buf, size, 0)) == err_code){
-		throw Ex1(Errcode::SOCKET_RECV);
-	}
-	return bytesRecv;
+int SocketIO::recv(socket_t sckt, const char *buf, int size, int flags) {
+  uint32_t bytesRecv;
+  if ((bytesRecv = ::recv(sckt, (char *)buf, size, 0)) == err_code) {
+    std::cout << WSAGetLastError() << std::endl;
+    throw Ex1(Errcode::SOCKET_RECV);
+  }
+  return bytesRecv;
 }
