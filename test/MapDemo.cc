de07086d168d4cc1e67e24e45fe76b9d2475e11f
Quentin Jimenez <qjimenez@stevens.edu>
2021-06-07 14:27:27 -0400

    Updated MapDemo

--- test/MapDemo.cc
+++ test/MapDemo.cc
@@ -11,10 +11,10 @@ using namespace grail;
 
 class TestMultiShape : public GLWin {
 public:
-	TestMultiShape() :
-		GLWin(0x000000, 0xCCCCCC, "Map Demo") {}
+const char* mapname;
+	TestMultiShape(const char* mapname) :
+		GLWin(0x000000, 0xCCCCCC, "Map Demo"), mapname(mapname){}
 
-#define FILENAME "shapefiles/Counties.txt"
 #define COLS 2 // Number of columns in data
 
 
@@ -25,89 +25,67 @@ public:
     gui->fillRectangle(100, 100, 100, 100, black);
 
 		fstream file;
-		vector <vector <float>> shapeVector;
-		float yMax = 0;
-		float xMax = 0;
-
+		vector <vector <float>> shapeVector; 
+		const float bigFloat = 1E20;
+		float yMax = -bigFloat;
+		float xMax = -bigFloat;
+		float yMin = bigFloat;
+		float xMin = bigFloat;
 		// Read file
-		file.open(FILENAME, ios::in); // Open file
+		file.open(mapname, ios::in); // Open file
 		if (file.is_open()) { // If file has correctly opened...
 			// Output debug message
 			cout << "File correctly opened" << endl;
 
+			string temp;
+			float x,y;
 			// Dynamically store data into array
 			while (file.peek()!=EOF) { // ... and while there are no errors,
-				string temp;
 				getline(file,temp,';');
+				shapeVector.push_back(vector<float>());
+				vector <float>& points = shapeVector.back();
 				stringstream ss(temp);
-				for(int i = 0; ss; i++){
-        	for(int k = 0; ss; k++){
-						shapeVector.push_back(vector<float>());
-						for(int j = 0; j < COLS; j++){
-							string s;
-							getline(ss,s,',');
-							int temp = abs(atof(s.c_str()));
-							//cout << temp << endl;
-							if(j%2 == 1){
-								if(temp > xMax){
-									xMax = temp;
-									
-									//cout << s << endl;
-									//cout << temp << " " << j << endl << endl;
-								}
-							}else{
-								if(temp > yMax){
-									//cout << s << endl;
-									//cout << temp << " " << j << endl << endl;
-									yMax = temp;
-								}
-							}
-							shapeVector[i].push_back(temp);
-							//cout << shapeVector[i][k] << endl;
-						}
-					}
+				while(ss>>x>>y){
+					points.push_back(x);
+					points.push_back(y);	
+					if(x > xMax) xMax = x;
+					if(y > yMax) yMax = y;
+					if(x < xMin) xMin = x;
+					if(y < yMin) yMin = y;
 				}
 			}
 		}
-
 		else cout << "Unable to open file" << endl;
 		file.close();
 
+		//cout << xMax << " " << yMax << " " << xMin << " " << yMin << endl;
 
 		double xSize = 1920;
 		double ySize = 1080;
-		int start = 1;
 
-		cout << xMax << " " << yMax << endl;
-	/*
-		for(int i = 0; i < 1;i++){
-			double max = *max_element(shapeVector[i].begin(), shapeVector[i].end());
-			for(int k = start;k < shapeVector[i].size()/2;k+=2){
-				//cout << shapeVector[i][k] << " " << shapeVector[i][k+1] << " " << yMax << endl;
-				//cout << shapeVector[i][k] << endl;
-				shapeVector[i][k] = ( xSize * (shapeVector[i][k] / xMax));
-				//cout << shapeVector[i][k] << endl << endl;
-				shapeVector[i][k+1] = ySize - ( ySize * (shapeVector[i][k+1] / yMax));
-				//cout << shapeVector[i][k] << " " << shapeVector[i][k+1] << endl << endl;
-				//cout << abs(shapeVector[i][k-2] - shapeVector[i][k]) << " " << shapeVector[i][k-2] << " " << shapeVector[i][k-1] << endl;
-				if(abs(shapeVector[i][k-2] - shapeVector[i][k]) < 1000 && shapeVector[i][k-2] > 0 && shapeVector[i][k-1] > 0){
-        	gui->drawLine(shapeVector[i][k-2],shapeVector[i][k-1],shapeVector[i][k],shapeVector[i][k+1],black); //TODO: Draw As Polygons
-				  cout << shapeVector[i][k-2] << "," << shapeVector[i][k-1] << " to " << shapeVector[i][k] << "," << shapeVector[i][k+1] << " Distance : "<< abs(shapeVector[i][k-2] - shapeVector[i][k]) << " Num : " << k <<  endl << endl;
-				}
-			}		
-		}
-*/
-		for(int i = 0; i < 1; i++){
-			for(int j = start; j < shapeVector[i].size()/2;j+=2){
-				shapeVector[i][j] = ( xSize * (shapeVector[i][j] / xMax));
-				shapeVector[i][j+1] = ySize - ( ySize * (shapeVector[i][j+1] / yMax));
+		double sX = (xSize-1)/(xMax-xMin);
+		double aX = (xSize-1)*(-xMin)/(xMax-xMin);
+
+		double sY = (ySize-1)/(yMax-yMin);
+		double aY = ySize- ((ySize-1)*(-yMin)/(yMax-yMin));
+
+		
+		for(int i = 0; i < shapeVector.size(); i++){
+			for(int j = 0; j < shapeVector[i].size();j+=2){
+				//shapeVector[i][j] = sX*shapeVector[i][j]+aX;
+				//shapeVector[i][j+1] = sY*shapeVector[i][j+1]+aY;
+				shapeVector[i][j] = (xSize*(shapeVector[i][j]/xMax));
+				shapeVector[i][j+1] = (ySize+300) - (ySize*(shapeVector[i][j+1]/yMax));
+				if(shapeVector[i][j] < 0 || shapeVector[i][j+1] < 0)
+					cout << shapeVector[i][j] << " " << shapeVector[i][j+1] << endl;
 			}
-			gui->drawPolygon(shapeVector[i],black); //TODO: Crashes, probably sending too big a vector
+			gui->drawPolygon(shapeVector[i],black); //TODO: Crashes
 		}
 
 	}
 };
 
 int main(int argc, char *argv[]) {
-	return GLWin::init(new TestMultiShape(),1920,1080);
+	const char* mapname = argc<2?"shapefiles/Counties.txt":argv[1];
+	return GLWin::init(new TestMultiShape(mapname),1920,1080);
 }
