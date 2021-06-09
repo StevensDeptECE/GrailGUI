bb88fcf423f84d3491cbe3ca928d81c4abff7ac4
Quentin Jimenez <qjimenez@stevens.edu>
2021-06-09 11:26:58 -0400

    Added stock tracker graph test

--- test/QuentinDemo.cc
+++ test/QuentinDemo.cc
@@ -1,140 +1,177 @@
-#include "opengl/GrailGUI.hh"
-#include"opengl/ButtonWidget.hh"
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include "opengl/ButtonWidget.hh"
+#include "opengl/GrailGUI.hh"
+#include "opengl/LineGraphWidget.hh"
 using namespace std;
 using namespace grail;
 
 class TestMultiShape : public GLWin {
-public:
-	TestMultiShape() :
-		GLWin(0x000000, 0xCCCCCC, "Test StyledMultiShape") {}
+ public:
+  TestMultiShape() : GLWin(0x000000, 0xCCCCCC, "Test StyledMultiShape") {}
 
+  void init() {
+    MainCanvas* c = currentTab()->getMainCanvas();
+    StyledMultiShape2D* gui = c->getGui();
+/*
+    const float boxSize = 75;
+    const float drawSize = (boxSize / 4) * 5;
 
+    float xy2[] = {boxSize + drawSize / 2, drawSize / 2, drawSize / 2, boxSize + drawSize / 2, 7 * boxSize + drawSize / 2, 5 * boxSize + drawSize / 2, 8 * boxSize + drawSize / 2, 4 * boxSize + drawSize / 2};
+    gui->fillPolygon(xy2, 4, blue);
 
-	void init() {
-		MainCanvas* c = currentTab()->getMainCanvas();
-		StyledMultiShape2D* gui = c->getGui();
+    gui->fillRectangle(boxSize, boxSize, drawSize, drawSize / 5, black);
+    gui->fillRectangle(boxSize, boxSize, drawSize / 5, drawSize, black);
+    gui->fillRectangle(boxSize, boxSize * 2, drawSize, drawSize / 5, black);
+    gui->fillRectangle(boxSize * 2, boxSize * 1.5, drawSize / 5, drawSize * .4, black);
+    gui->fillRectangle(boxSize * 1.5, boxSize * 1.5, drawSize * .4, drawSize / 5, black);
 
-    
+    gui->fillRectangle(boxSize * 2.5, boxSize * 1.75, drawSize, drawSize / 5, black);
+    gui->fillRectangle(boxSize * 2.5, boxSize * 1.75, drawSize / 5, drawSize, black);
+    gui->fillRectangle(boxSize * 2.5, boxSize * 2.25, drawSize, drawSize / 5, black);
+    gui->fillRectangle(boxSize * 3.5, boxSize * 1.75, drawSize / 5, drawSize * .4, black);
+    gui->fillRectangle(boxSize * 2.75, boxSize * 2.5, drawSize * .4, drawSize / 5, black);
+    gui->fillRectangle(boxSize * 3.25, boxSize * 2.75, drawSize * .4, drawSize / 5, black);
 
-		const float boxSize = 75;
-		const float drawSize = (boxSize/4)*5;
+    gui->fillRectangle(boxSize * 4, boxSize * 2.5, drawSize, drawSize / 5, black);
+    gui->fillRectangle(boxSize * 4, boxSize * 2.5, drawSize / 5, drawSize, black);
+    gui->fillRectangle(boxSize * 5, boxSize * 2.5, drawSize / 5, drawSize, black);
+    gui->fillRectangle(boxSize * 4, boxSize * 3, drawSize, drawSize / 5, black);
 
-    float xy2[] = {boxSize+drawSize/2, drawSize/2, drawSize/2, boxSize+drawSize/2, 7*boxSize+drawSize/2, 5*boxSize+drawSize/2, 8*boxSize+drawSize/2, 4*boxSize+drawSize/2};
-		gui->fillPolygon(xy2, 4, blue);
-    
-    
-    gui->fillRectangle(boxSize, boxSize, drawSize, drawSize/5, black);
-    gui->fillRectangle(boxSize, boxSize, drawSize/5, drawSize, black);
-    gui->fillRectangle(boxSize, boxSize*2, drawSize, drawSize/5, black);
-    gui->fillRectangle(boxSize*2, boxSize*1.5, drawSize/5, drawSize*.4, black);
-    gui->fillRectangle(boxSize*1.5, boxSize*1.5, drawSize*.4, drawSize/5, black);
+    gui->fillRectangle(boxSize * 5.5, boxSize * 3.25, drawSize, drawSize / 5, black);
+    gui->fillRectangle(boxSize * 5.5, boxSize * 4.25, drawSize, drawSize / 5, black);
+    gui->fillRectangle(boxSize * 6, boxSize * 3.25, drawSize / 5, drawSize, black);
 
-    gui->fillRectangle(boxSize*2.5, boxSize*1.75, drawSize, drawSize/5, black);
-    gui->fillRectangle(boxSize*2.5, boxSize*1.75, drawSize/5, drawSize, black);
-    gui->fillRectangle(boxSize*2.5, boxSize*2.25, drawSize, drawSize/5, black);
-    gui->fillRectangle(boxSize*3.5, boxSize*1.75, drawSize/5, drawSize*.4, black);
-    gui->fillRectangle(boxSize*2.75, boxSize*2.5, drawSize*.4, drawSize/5, black);
-    gui->fillRectangle(boxSize*3.25, boxSize*2.75, drawSize*.4, drawSize/5, black);
-    
+    gui->fillRectangle(boxSize * 7, boxSize * 4, drawSize / 5, drawSize, black);
+    gui->fillRectangle(boxSize * 7, boxSize * 5, drawSize, drawSize / 5, black);
 
-    gui->fillRectangle(boxSize*4, boxSize*2.5, drawSize, drawSize/5, black);
-    gui->fillRectangle(boxSize*4, boxSize*2.5, drawSize/5, drawSize, black);
-    gui->fillRectangle(boxSize*5, boxSize*2.5, drawSize/5, drawSize, black);
-    gui->fillRectangle(boxSize*4, boxSize*3, drawSize, drawSize/5, black);
+    glm::vec4 v = {1, 2, 3, 4};
+    float w = 7;
 
-    gui->fillRectangle(boxSize*5.5, boxSize*3.25, drawSize, drawSize/5, black);
-    gui->fillRectangle(boxSize*5.5, boxSize*4.25, drawSize, drawSize/5, black);
-    gui->fillRectangle(boxSize*6, boxSize*3.25, drawSize/5, drawSize, black);
-   
-    gui->fillRectangle(boxSize*7, boxSize*4, drawSize/5, drawSize, black);
-    gui->fillRectangle(boxSize*7, boxSize*5, drawSize, drawSize/5, black);
-
-    glm::vec4 v = {1,2,3,4};
-    float w  = 7;
-
-    gui->drawLine(w*31,w*85.002,w*30.997,w*87.598,black);
-    gui->drawLine(w*30.997,w*87.598,w*30.846,w*87.625,black);
-    gui->drawLine(w*30.846,w*87.625,w*30.655,w*87.397,black);
-    gui->drawLine(w*30.655,w*87.397,w*30.466,w*87.426,black);
+    gui->drawLine(w * 31, w * 85.002, w * 30.997, w * 87.598, black);
+    gui->drawLine(w * 30.997, w * 87.598, w * 30.846, w * 87.625, black);
+    gui->drawLine(w * 30.846, w * 87.625, w * 30.655, w * 87.397, black);
+    gui->drawLine(w * 30.655, w * 87.397, w * 30.466, w * 87.426, black);
     //gui->drawLine(w*30.466,w*87.426,w*31,w*85.002,black);
     //gui->drawLine(w*34.995,w*88.2,w*34.984,w*85.605,black);
-gui->drawLine(w*34.984,w*85.605,w*32.864,w*85.184,black);
-gui->drawLine(w*32.864,w*85.184,w*32.426,w*84.964,black);
-gui->drawLine(w*32.426,w*84.964,w*32.326,w*85.006,black);
-gui->drawLine(w*32.326,w*85.006,w*32.261,w*84.889,black);
-gui->drawLine(w*32.261,w*84.889,w*32.133,w*85.062,black);
-gui->drawLine(w*32.133,w*85.062,w*31.84,w*85.141,black);
-gui->drawLine(w*31.84,w*85.141,w*31.538,w*85.041,black);
-gui->drawLine(w*31.538,w*85.041,w*31.188,w*85.108,black);
-gui->drawLine(w*31.188,w*85.108,w*31,w*85.002,black);
+    gui->drawLine(w * 34.984, w * 85.605, w * 32.864, w * 85.184, black);
+    gui->drawLine(w * 32.864, w * 85.184, w * 32.426, w * 84.964, black);
+    gui->drawLine(w * 32.426, w * 84.964, w * 32.326, w * 85.006, black);
+    gui->drawLine(w * 32.326, w * 85.006, w * 32.261, w * 84.889, black);
+    gui->drawLine(w * 32.261, w * 84.889, w * 32.133, w * 85.062, black);
+    gui->drawLine(w * 32.133, w * 85.062, w * 31.84, w * 85.141, black);
+    gui->drawLine(w * 31.84, w * 85.141, w * 31.538, w * 85.041, black);
+    gui->drawLine(w * 31.538, w * 85.041, w * 31.188, w * 85.108, black);
+    gui->drawLine(w * 31.188, w * 85.108, w * 31, w * 85.002, black);
 
-gui->drawLine(w*34.995,w*88.2,w*34.984,w*85.605,black);
+    gui->drawLine(w * 34.995, w * 88.2, w * 34.984, w * 85.605, black);
 
-//gui->drawLine(w*31,w*85.002,w*30.348,w*88.394,black);
-gui->drawLine(w*30.348,w*88.394,w*31.894,w*88.473,black);
-gui->drawLine(w*31.894,w*88.473,w*32.732,w*88.37,black);
-gui->drawLine(w*32.732,w*88.37,w*32.747,w*88.368,black);
-gui->drawLine(w*32.747,w*88.368,w*34.892,w*88.097,black);
-gui->drawLine(w*34.892,w*88.097,w*34.995,w*88.2,black);
-gui->drawLine(w*34.995,w*88.2,w*34.995,w*88.2,black);
+    //gui->drawLine(w*31,w*85.002,w*30.348,w*88.394,black);
+    gui->drawLine(w * 30.348, w * 88.394, w * 31.894, w * 88.473, black);
+    gui->drawLine(w * 31.894, w * 88.473, w * 32.732, w * 88.37, black);
+    gui->drawLine(w * 32.732, w * 88.37, w * 32.747, w * 88.368, black);
+    gui->drawLine(w * 32.747, w * 88.368, w * 34.892, w * 88.097, black);
+    gui->drawLine(w * 34.892, w * 88.097, w * 34.995, w * 88.2, black);
+    gui->drawLine(w * 34.995, w * 88.2, w * 34.995, w * 88.2, black);
 
-////
+    ////
 
-gui->drawLine(w*33.637,w*94.485,w*33.961,w*95.227,black);
-gui->drawLine(w*33.961,w*95.227,w*33.873,w*95.28,black);
-gui->drawLine(w*33.873,w*95.28,w*33.939,w*95.6,black);
-gui->drawLine(w*33.939,w*95.6,w*33.845,w*95.776,black);
-gui->drawLine(w*33.845,w*95.776,w*33.838,w*96.147,black);
-gui->drawLine(w*33.838,w*96.147,w*33.687,w*96.352,black);
-gui->drawLine(w*33.687,w*96.352,w*33.843,w*96.625,black);
-gui->drawLine(w*33.843,w*96.625,w*33.894,w*96.587,black);
-gui->drawLine(w*33.894,w*96.587,w*33.823,w*96.758,black);
-gui->drawLine(w*33.823,w*96.758,w*33.955,w*96.985,black);
-gui->drawLine(w*33.955,w*96.985,w*33.718,w*97.135,black);
-gui->drawLine(w*33.718,w*97.135,w*33.914,w*97.218,black);
-gui->drawLine(w*33.914,w*97.218,w*33.819,w*97.427,black);
-gui->drawLine(w*33.819,w*97.427,w*33.991,w*97.669,black);
-gui->drawLine(w*33.991,w*97.669,w*33.856,w*97.84,black);
-gui->drawLine(w*33.856,w*97.84,w*33.881,w*97.97,black);
-gui->drawLine(w*33.881,w*97.97,w*33.989,w*97.946,black);
-gui->drawLine(w*33.989,w*97.946,w*34.011,w*98.091,black);
-gui->drawLine(w*34.011,w*98.091,w*34.153,w*98.117,black);
-gui->drawLine(w*34.153,w*98.117,w*34.148,w*98.35,black);
-gui->drawLine(w*34.148,w*98.35,w*34.057,w*98.458,black);
-gui->drawLine(w*34.057,w*98.458,w*34.161,w*98.631,black);
-gui->drawLine(w*34.161,w*98.631,w*34.114,w*98.741,black);
-gui->drawLine(w*34.114,w*98.741,w*34.213,w*99.186,black);
-gui->drawLine(w*34.213,w*99.186,w*34.453,w*99.361,black);
-gui->drawLine(w*34.453,w*99.361,w*34.373,w*99.405,black);
-gui->drawLine(w*34.373,w*99.405,w*34.381,w*99.71,black);
-gui->drawLine(w*34.381,w*99.71,w*34.557,w*99.998,black);
-gui->drawLine(w*34.557,w*99.998,w*36.5,w*100.003,black);
-gui->drawLine(w*36.5,w*100.003,w*36.5,w*103.001,black);
+    gui->drawLine(w * 33.637, w * 94.485, w * 33.961, w * 95.227, black);
+    gui->drawLine(w * 33.961, w * 95.227, w * 33.873, w * 95.28, black);
+    gui->drawLine(w * 33.873, w * 95.28, w * 33.939, w * 95.6, black);
+    gui->drawLine(w * 33.939, w * 95.6, w * 33.845, w * 95.776, black);
+    gui->drawLine(w * 33.845, w * 95.776, w * 33.838, w * 96.147, black);
+    gui->drawLine(w * 33.838, w * 96.147, w * 33.687, w * 96.352, black);
+    gui->drawLine(w * 33.687, w * 96.352, w * 33.843, w * 96.625, black);
+    gui->drawLine(w * 33.843, w * 96.625, w * 33.894, w * 96.587, black);
+    gui->drawLine(w * 33.894, w * 96.587, w * 33.823, w * 96.758, black);
+    gui->drawLine(w * 33.823, w * 96.758, w * 33.955, w * 96.985, black);
+    gui->drawLine(w * 33.955, w * 96.985, w * 33.718, w * 97.135, black);
+    gui->drawLine(w * 33.718, w * 97.135, w * 33.914, w * 97.218, black);
+    gui->drawLine(w * 33.914, w * 97.218, w * 33.819, w * 97.427, black);
+    gui->drawLine(w * 33.819, w * 97.427, w * 33.991, w * 97.669, black);
+    gui->drawLine(w * 33.991, w * 97.669, w * 33.856, w * 97.84, black);
+    gui->drawLine(w * 33.856, w * 97.84, w * 33.881, w * 97.97, black);
+    gui->drawLine(w * 33.881, w * 97.97, w * 33.989, w * 97.946, black);
+    gui->drawLine(w * 33.989, w * 97.946, w * 34.011, w * 98.091, black);
+    gui->drawLine(w * 34.011, w * 98.091, w * 34.153, w * 98.117, black);
+    gui->drawLine(w * 34.153, w * 98.117, w * 34.148, w * 98.35, black);
+    gui->drawLine(w * 34.148, w * 98.35, w * 34.057, w * 98.458, black);
+    gui->drawLine(w * 34.057, w * 98.458, w * 34.161, w * 98.631, black);
+    gui->drawLine(w * 34.161, w * 98.631, w * 34.114, w * 98.741, black);
+    gui->drawLine(w * 34.114, w * 98.741, w * 34.213, w * 99.186, black);
+    gui->drawLine(w * 34.213, w * 99.186, w * 34.453, w * 99.361, black);
+    gui->drawLine(w * 34.453, w * 99.361, w * 34.373, w * 99.405, black);
+    gui->drawLine(w * 34.373, w * 99.405, w * 34.381, w * 99.71, black);
+    gui->drawLine(w * 34.381, w * 99.71, w * 34.557, w * 99.998, black);
+    gui->drawLine(w * 34.557, w * 99.998, w * 36.5, w * 100.003, black);
+    gui->drawLine(w * 36.5, w * 100.003, w * 36.5, w * 103.001, black);
 
-gui->drawLine(w*36.499,w*94.617,w*35.398,w*94.431,black);
-gui->drawLine(w*35.398,w*94.431,w*33.637,w*94.485,black);
+    gui->drawLine(w * 36.499, w * 94.617, w * 35.398, w * 94.431, black);
+    gui->drawLine(w * 35.398, w * 94.431, w * 33.637, w * 94.485, black);
 
-gui->drawLine(w*36.998,w*94.618,w*36.499,w*94.617,black);
+    gui->drawLine(w * 36.998, w * 94.618, w * 36.499, w * 94.617, black);
 
-gui->drawLine(w*36.998,w*94.618,w*36.993,w*102.041,black);
+    gui->drawLine(w * 36.998, w * 94.618, w * 36.993, w * 102.041, black);
 
+    gui->drawLine(w * 37, w * 103.001, w * 36.993, w * 102.041, black);
 
-gui->drawLine(w*37,w*103.001,w*36.993,w*102.041,black);
-
-gui->drawLine(w*37,w*103.001,w*36.5,w*103.001,black);
-
-
-
+    gui->drawLine(w * 37, w * 103.001, w * 36.5, w * 103.001, black);
 
     const char buttonName[] = "button";
+    */
     MultiText* guiText = c->getGuiText();
     //guiText->add(300,100,s,1);
     ButtonWidget b(gui, guiText, "hello", 0, 0, 100, 50);
-    b.init();
-	}
+    //b.init();
+
+
+
+    
+    fstream file;
+		vector <float> dailyMaximum; 
+		const float bigFloat = 1E20;
+		float yMax = -bigFloat;
+		float xMax = -bigFloat;
+		float yMin = bigFloat;
+		float xMin = bigFloat;
+		// Read file
+		file.open("GME.csv", ios::in); // Open file
+		if (file.is_open()) { // If file has correctly opened...
+			// Output debug message
+			cout << "File correctly opened" << endl;
+
+			string temp;
+			float x,y;
+			// Dynamically store data into array
+			while (file.peek()!=EOF) { // ... and while there are no errors,
+				getline(file,temp,'\n');
+				dailyMaximum.push_back(atof(temp.c_str()));
+			}
+		}
+		else cout << "Unable to open file" << endl;
+		file.close();
+
+
+  vector<float> day; 
+  for(int i = 0; i < dailyMaximum.size(); i++)
+    day.push_back(i);
+
+
+
+    LineGraphWidget chart(gui, guiText, 50, 320, 800, 400);
+    chart.chart(day, dailyMaximum, 50, 50, new LinearScale(), new LinearScale());
+    chart.title("Daily High of GME Stock Since 2010");
+    chart.init();
+
+
+  }
 };
 
-int main(int argc, char *argv[]) {
-	return GLWin::init(new TestMultiShape());
+int main(int argc, char* argv[]) {
+  return GLWin::init(new TestMultiShape());
 }
