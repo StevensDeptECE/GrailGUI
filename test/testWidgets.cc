066a6bb2d38ea6d179cd96504db78fcf444b732d
Luisa Bonfim <lbonfim@stevens.edu>
2021-06-14 14:34:01 -0400

    GapMinderWidget changes

--- test/testWidgets.cc
+++ test/testWidgets.cc
@@ -80,15 +80,16 @@ class TestWidgets : public GLWin {
   }
 
   void testGapMinder(StyledMultiShape2D *gui, MultiText *guiText){
-    vector<float> x = {100, 10, 50, 150, 300};
-    vector<float> y = {150, 350, 222, 100, 300};
+    vector<float> x = {100, 220, 50, 150, 300, 290, 230};
+    vector<float> y = {150, 350, 222, 100, 290, 60, 200};
+    vector<float> s = {3, 2, 6, 5, 4, 10, 15};
+    vector<glm::vec4> c = {grail::red, grail::blue, grail::green, grail:: cyan, grail::purple, grail::darkblue};
     
-    GapMinderWidget chart(gui, guiText, 550, 320, 400, 200, y, x);
-    chart.chart(y, x, 50);
+    GapMinderWidget chart(gui, guiText, 550, 320, 400, 200, x, y);
+    chart.chart(y, x, s, 50, c);
     chart.setTitle("Title");
     chart.init();
   }
-
 
   void testLineGraphLinear(StyledMultiShape2D *gui, MultiText *guiText) {
     vector<float> x = {20, 40, 100, 200};
@@ -117,13 +118,11 @@ class TestWidgets : public GLWin {
     scrollBar.init();
   }
 
-  void testAngleText(StyledMultiShape2D *gui, MultiText *guiText, Canvas *c,
-                     const Style *s) {
+  void testAngleText(StyledMultiShape2D *gui, MultiText *guiText, Canvas *c, const Style *s) {
     const char thing[] = "hello world";
-    guiText->add(0, 50, s->f, thing, strlen(thing));
-    AngledMultiText *am =
-        c->addLayer(new AngledMultiText(c, s, M_PI / 4, 20, 500));
-    am->add(0, 0, s->f, thing, strlen(thing));
+    //guiText->add(0, 50, s->f, thing, strlen(thing));
+    AngledMultiText *am = c->addLayer(new AngledMultiText(c, s, M_PI / 2, 20, 500));
+    am->add(200, 600, s->f, thing, strlen(thing));
   }
 
 #if 0
@@ -160,15 +159,15 @@ class TestWidgets : public GLWin {
     MultiText *guiText = c->addLayer(new MultiText(c, s));
 
     const Style *graphStyle = new Style("TIMES", 12, 1, 0, 0, 0, 0, 0, 0);
-    testBarChart(gui, guiText);
+    //testBarChart(gui, guiText);
     testCandlestick(gui, guiText);
     testBoxChart(gui, guiText);
-    //testGapMinder(gui, guiText);
+    testGapMinder(gui, guiText);
     //testButton(gui, guiText);
     //testLineGraphLinear(gui, guiText);
     //testLineGraphLog(gui, guiText);
     //testLinearAxesWidget(gui, guiText, graphStyle);
-
+    //testAngleText(gui, guiText, c, s);
     testScrollBar(gui, guiText);
   }
 };
