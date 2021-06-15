33d7c316a0e916f2e7ede8c9e862dd2ca78efcb8
Luisa Bonfim <lbonfim@stevens.edu>
2021-06-15 19:06:28 -0400

    changes in GapMinder and added Sparkline

---
+++ src/opengl/SparklineWidget.cc
@@ -0,0 +1,39 @@
+#include "opengl/SparklineWidget.hh"
+#include "opengl/StyledMultiShape2D.hh"
+#include "opengl/MultiText.hh"
+#include <string>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+void SparklineWidget::init() {
+  m ->drawRectangle(x, y, w, h, grail::black);
+}
+
+void SparklineWidget::setTitle(const string& s){
+  const Font* f = FontFace::get("TIMES", 12, FontFace::BOLD);
+    t->add(x, y-10, f , s.c_str(), s.length()); 
+}
+
+/*
+
+*/
+void SparklineWidget::chart(const vector<float>& yLocations, 
+    const vector<float>& xLocations, glm::vec4& c){
+
+    yAxis->init(minY, maxY, y+h, -h, 0);
+    xAxis->init(minX, maxX, x, w, 0);
+
+    const Font* f = FontFace::get("TIMES", 12, FontFace::BOLD);
+
+    for (int i = 1; i < xLocations.size(); i++) {
+      float xPoint1 = xAxis->transform(xLocations[i-1]);
+      float yPoint1 = yAxis->transform(yLocations[i-1]);
+      float xPoint2 = xAxis->transform(xLocations[i]);
+      float yPoint2 = yAxis->transform(yLocations[i]);
+
+      m->drawLine(xPoint1, yPoint1, xPoint2, yPoint2, c);
+    }
+
+  }
