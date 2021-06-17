20d7c281802c6f4a3f0115b96298ea219075df15
David Krauthamer <david_krauthamer@protonmail.com>
2021-06-17 16:06:29 -0400

    Create basic wrapper around mpv

--- src/audio/AudioPlayer.cc
+++ src/audio/AudioPlayer.cc
@@ -1,1 +1,33 @@
 #include "AudioPlayer.hh"
+
+using namespace std;
+AudioPlayer::AudioPlayer(string filePath, int volume)
+    : filePath(filePath), volume(volume) {}
+
+void AudioPlayer::setFilePath(string filePath) { this->filePath = filePath; }
+
+void AudioPlayer::setVolume(int volume) { this->volume = volume; }
+
+void AudioPlayer::init() {
+  ctx = mpv_create();
+  if (!ctx) {
+    printf("failed creating context\n");
+    // todo, crash / fail here
+  }
+
+  check_error(mpv_initialize(ctx));
+  check_error(mpv_command_string(ctx, "cycle pause"));
+
+  const char *loadCmd[] = {"loadfile", filePath.c_str(), NULL};
+  check_error(mpv_command(ctx, loadCmd));
+
+  char volumeCmd[25];
+  sprintf(volumeCmd, "set volume %d", volume);
+  check_error(mpv_command_string(ctx, volumeCmd));
+}
+
+void AudioPlayer::togglePause() {
+  check_error(mpv_command_string(ctx, "cycle pause"));
+}
+
+void AudioPlayer::destroy() { mpv_terminate_destroy(ctx); }
--- src/audio/AudioPlayer.hh
+++ src/audio/AudioPlayer.hh
@@ -1,17 +1,34 @@
-// Build with: gcc -o simple simple.c `pkg-config --libs --cflags mpv`
-
 #include <mpv/client.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-static inline void check_error(int status) {
-  if (status < 0) {
-    printf("mpv API error: %s\n", mpv_error_string(status));
-    exit(1);
-  }
-}
+#include <string>
 
+class AudioPlayer {
+ private:
+  std::string filePath;
+  int volume;
+  mpv_handle *ctx;
+
+  static inline void check_error(int status) {
+    if (status < 0) {
+      printf("mpv API error: %s\n", mpv_error_string(status));
+      exit(1);
+    }
+  }
+
+ public:
+  AudioPlayer(std::string filePath = "", int volume = 75);
+  void setFilePath(std::string filePath);
+  // needs to check if between 0 and 100 (or a reasonable bound)
+  void setVolume(int volume);
+  void init();
+  void togglePause();
+  void destroy();
+};
+
+#if 0
 int main(int argc, char *argv[]) {
   const char *vol;
 
@@ -26,37 +43,32 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  mpv_handle *ctx = mpv_create();
-  if (!ctx) {
-    printf("failed creating context\n");
-    return 1;
-  }
-
   // Enable default key bindings, so the user can actually interact with
   // the player (and e.g. close the window).
-  check_error(mpv_set_option_string(ctx, "input-default-bindings", "yes"));
-  mpv_set_option_string(ctx, "input-vo-keyboard", "yes");
-  int val = 1;
-  check_error(mpv_set_option(ctx, "osc", MPV_FORMAT_FLAG, &val));
+  // check_error(mpv_set_option_string(ctx, "input-default-bindings", "yes"));
+  // mpv_set_option_string(ctx, "input-vo-keyboard", "yes");
+  // int val = 1;
+  // check_error(mpv_set_option(ctx, "osc", MPV_FORMAT_FLAG, &val));
 
   // Done setting up options.
-  check_error(mpv_initialize(ctx));
+  // check_error(mpv_initialize(ctx));
 
   // Play this file.
-  const char *cmd[] = {"loadfile", argv[1], NULL};
-  check_error(mpv_command(ctx, cmd));
+  // const char *cmd[] = {"loadfile", argv[1], NULL};
+  // check_error(mpv_command(ctx, cmd));
 
-  char volcmd[20];
-  sprintf(volcmd, "set volume %s", vol);
-  check_error(mpv_command_string(ctx, volcmd));
+  // char volcmd[20];
+  // sprintf(volcmd, "set volume %s", vol);
+  // check_error(mpv_command_string(ctx, volcmd));
 
   // Let it play, and wait until the user quits.
-  while (1) {
-    mpv_event *event = mpv_wait_event(ctx, 10000);
-    printf("event: %s\n", mpv_event_name(event->event_id));
-    if (event->event_id == MPV_EVENT_SHUTDOWN) break;
-  }
+  // while (1) {
+  //   mpv_event *event = mpv_wait_event(ctx, 10000);
+  //   printf("event: %s\n", mpv_event_name(event->event_id));
+  //   if (event->event_id == MPV_EVENT_SHUTDOWN) break;
+  // }
 
   mpv_terminate_destroy(ctx);
   return 0;
 }
+#endif
