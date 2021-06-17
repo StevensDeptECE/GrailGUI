b5361d45fe4a32ff127a0eb816af685cdef03758
Dov Kruger <Dov.Kruger@stevens.edu>
2021-06-17 10:36:16 -0400

    new directory for any code purely loading/saving data, no graphics

---
+++ test/data/testBlockMapLoader.cc
@@ -0,0 +1,10 @@
+#include "data/BlockMapLoader.hh"
+
+int main(int argc, char* argv[]) {
+  const char* shapefilename =
+      argc > 1 ? argv[1] : "res/us_counties/USA_Counties.shp";
+  int seg = argc > 2 ? atoi(argv[2]) : -1;
+  BlockMapLoader bml(shapefilename, "ESRI");
+  if (seg >= 0) bml.dumpSegment(seg);
+  bml.save("uscounties.bml");
+}
