59601c1551292ae352798804152a29dfbeddfd31
ahuston-0 <aliceghuston@gmail.com>
2021-06-25 17:09:22 -0400

    Improve build script

--- build.sh
+++ build.sh
@@ -1,10 +1,29 @@
 #!/usr/bin/env bash
 
+if [[ $(pwd | awk -F"/" '{print $NF}') == "test" ]]; then
+	cd ..
+elif [[ -z "$GRAIL" ]]; then # Currently does not find GRAIL
+	cd "$GRAIL" || exit 1
+fi
+
 if [ -d "build" ]; then
 	cd "build" || exit 1
-	pwd
+else
+	if [[ -f "CMakeLists.txt" ]]; then
+		mkdir build
+		cd "build" || exit 1
+	else
+		{
+			echo "Error: no CMakeLists.txt, no build directory"
+			echo "Are you in the right directory?"
+		} >/dev/stderr
+		exit 1
+	fi
+fi
+
+if [ -f "build.ninja" ]; then
 	ninja
 else
-	echo "build directory \"build\" does not exist"
-	echo "Please run ./configure.sh"
+	cmake -GNinja ..
+	ninja
 fi
