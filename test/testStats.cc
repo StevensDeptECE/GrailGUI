b8f84df8e4dcfbe61dc81fc9eb93e0604e31bfd6
David Krauthamer <david_krauthamer@protonmail.com>
2021-08-09 11:08:18 -0400

    Update test to utilize new changes

--- test/testStats.cc
+++ test/testStats.cc
@@ -4,7 +4,7 @@
 
 #include "xp/Stats.hh"
 
-using stats::QuantileAlgorithm;
+using namespace stats::aliases;
 
 using namespace std;
 
@@ -18,35 +18,35 @@ int main() {
                          180, 300, 49,  247, 325, 114, 89,  69};
 
   double mean = stats::mean(data);
+  double median = stats::median(data);
   double pstdev = stats::pstdev(data, mean);
   double stdev = stats::stdev(data, mean);
   double pvariance = stats::pvariance(data, mean);
   double variance = stats::variance(data, mean);
 
   cout << mean << endl
+       << median << endl
        << pstdev << endl
        << stdev << endl
        << pvariance << endl
-       << variance << endl;
+       << variance << endl
+       << endl;
 
   assert(compare_double(mean, 186.731, 0.001));
+  assert(compare_double(median, 185, 0.01));
   assert(compare_double(pstdev, 95.3574, 0.0001));
   assert(compare_double(stdev, 97.2459, 0.0001));
   assert(compare_double(pvariance, 9093.04, 0.01));
   assert(compare_double(variance, 9456.76, 0.01));
 
   double q1 = stats::quantile(data, .25, QuantileAlgorithm::R5);
-  // assert(is_between(q1, 100, 100));
+  double q3 = stats::quantile(data, .75, QuantileAlgorithm::R5);
 
-  // assert(is_between(stats::quantile(data, .75, stats::QuantileAlgorithm::R5),
-  //                   270, 270));
+  cout << q1 << endl << q3 << endl << endl;
 
-  // double median =
-  //     stats::five_number_summary(data, stats::QuantileAlgorithm::R5).median;
-  // assert(is_between(median, 185, 185));
+  assert(compare_double(q1, 100));
+  assert(compare_double(q3, 270));
 
-  // assert(is_between(stats::iqr(data, stats::QuantileAlgorithm::R5), 169.99,
-  //                   170.1));
-
+  cout << stats::mode(data) << endl;
   for (auto const& n : stats::multimode(data)) cout << n << endl;
 }
