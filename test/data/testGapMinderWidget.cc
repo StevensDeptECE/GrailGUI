50d9339b6594d8b4b5ed415fde5abf1c902fffad
Dov Kruger <Dov.Kruger@stevens.edu>
2021-08-20 12:25:34 -0400

    animation works again for solar system, all graphs not animated by default. Need alternate constructor to allow graphs to request animation when they want it

---
+++ test/data/testGapMinderWidget.cc
@@ -0,0 +1,49 @@
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include "opengl/GraphStyle.hh"
+#include "opengl/GapMinderWidget.hh"
+#include "data/GapMinderLoader.hh"
+#include "opengl/GrailGUI.hh"
+
+using namespace std;
+using namespace grail;
+
+
+class TestGapMinderWidget : public GraphStyle {
+ private:
+  GapMinderWidget* chart;
+ public:
+  
+  TestGapMinderWidget(Tab* tab) : GraphStyle(tab, "TIMES", 24, 12) {
+    MainCanvas *c = tab->getMainCanvas();
+    const Style *s = new Style("TIMES", 24, 1, 0, 0, 0,  // black background (unused)
+                  0, 0, 0);                 // black foreground text
+    StyledMultiShape2D *gui = c->getGui();
+    MultiText *guiText = c->addLayer(new MultiText(c, s));
+    string xData = "gdp_per_capita.csv";
+    string yData = "ddf--datapoints--poisonings_deaths_per_100000_people.csv";
+    string sData = "ddf--datapoints--vacc_rate--by--country--time.csv";
+
+    vector <float> x2 = {65000};
+    vector <float> y2 = {14};
+    chart = new GapMinderWidget(gui, guiText, 75, 50, 900, 450, x2, y2);
+    chart->loadData(yData, xData, sData, 2000, 2010);
+
+    chart->setTitle("GDP per Capita vs. Poisonings per 100k");
+    chart->init();
+  }
+
+  ~TestGapMinderWidget() { delete chart; }
+  TestGapMinderWidget(const TestGapMinderWidget& orig) = delete;
+  TestGapMinderWidget& operator =(const TestGapMinderWidget& orig) = delete;
+
+  void update(){
+    chart->animate(10000, 2);
+  }
+};
+
+void grailmain(int argc, char *argv[], GLWin* w, Tab* tab) {
+  w->setTitle("Test Gap Minder Widget");
+  tab->addAnimated(new TestGapMinderWidget(tab));
+}
