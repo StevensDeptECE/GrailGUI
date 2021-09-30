45a1dd637ce457145473b0df86911e5831d0adf1
Tyler Kaminski <durcor@disroot.org>
2021-09-30 16:35:00 -0400

    Clean up

--- src/repo/GrailRepository.cc
+++ src/repo/GrailRepository.cc
@@ -11,27 +11,27 @@ void generateKey(const std::string& siteName)
     BIGNUM *bne = NULL;
     BIO *bp_public = NULL, *bp_private = NULL;
     int bits = 2048;
-	unsigned long e = RSA_F4;
+    unsigned long e = RSA_F4;
 
     bne = BN_new();
-	ret = BN_set_word(bne, e);
-	if (ret != 1)
+    ret = BN_set_word(bne, e);
+    if (ret != 1)
         std::cerr << "Error: Creating bignum failed." << std::endl;
 
-	r = RSA_new();
-	ret = RSA_generate_key_ex(r, bits, bne, NULL);
-	if (ret != 1)
+    r = RSA_new();
+    ret = RSA_generate_key_ex(r, bits, bne, NULL);
+    if (ret != 1)
         std::cerr << "Error: Generating key pair failed." << std::endl;
 
     // XXX: Should we save keys to disk?
-	bp_public = BIO_new_file("public.pem", "w+");
-	ret = PEM_write_bio_RSAPublicKey(bp_public, r);
-	if (ret != 1)
+    bp_public = BIO_new_file("public.pem", "w+");
+    ret = PEM_write_bio_RSAPublicKey(bp_public, r);
+    if (ret != 1)
         std::cerr << "Error: Writing public key failed." << std::endl;
 
-	bp_private = BIO_new_file("private.pem", "w+");
-	ret = PEM_write_bio_RSAPrivateKey(bp_private, r, NULL, NULL, 0, NULL, NULL);
-	if (ret != 1)
+    bp_private = BIO_new_file("private.pem", "w+");
+    ret = PEM_write_bio_RSAPrivateKey(bp_private, r, NULL, NULL, 0, NULL, NULL);
+    if (ret != 1)
         std::cerr << "Error: Writing private key failed." << std::endl;
 
     // Couple each site with its keypair.
