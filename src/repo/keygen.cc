b070a063ba923096cb190f932f4a1ef6305458f1
Tyler Kaminski <durcor@disroot.org>
2021-10-17 14:57:47 -0400

    Clean up key generation, work on building up submodules

--- src/repo/keygen.cc
+++ src/repo/keygen.cc
@@ -6,24 +6,21 @@ std::vector<std::pair<std::string, RSA *>> siteKeys;
 
 void generateKey(const std::string& siteName)
 {
-    int ret = 0;
-    RSA *r = NULL;
-    BIGNUM *bne = NULL;
-    BIO *bp_public = NULL, *bp_private = NULL;
     int bits = 2048;
     unsigned long e = RSA_F4;
 
-    bne = BN_new();
-    ret = BN_set_word(bne, e);
+    BIGNUM *bne = BN_new();
+    int ret = BN_set_word(bne, e);
     if (ret != 1)
         std::cerr << "Error: Creating bignum failed." << std::endl;
 
-    r = RSA_new();
+    RSA *r = RSA_new();
     ret = RSA_generate_key_ex(r, bits, bne, NULL);
     if (ret != 1)
         std::cerr << "Error: Generating key pair failed." << std::endl;
 
     // XXX: Should we save keys to disk?
+    BIO *bp_public = NULL, *bp_private = NULL;
     bp_public = BIO_new_file("public.pem", "w+");
     ret = PEM_write_bio_RSAPublicKey(bp_public, r);
     if (ret != 1)
@@ -54,23 +51,19 @@ void backupToCloud(const std::string& service_name, const std::string& userid,
         const std::string& passwd, uint32_t offset, uint32_t stride,
         uint32_t bits);
 
-void restoryFromCloud(const std::string& service_name,
+void restoreFromCloud(const std::string& service_name,
         const std::string& userid, const std::string& passwd, uint32_t offset,
         uint32_t stride, uint32_t bits);
 
-void unlock(const std::string& password, const Factor2& factor,
-        const std::string& area); 
+// void unlock(const std::string& password, const Factor2& factor,
+//         const std::string& area); 
 
 void scramble(const std::string& area);
   
 void destroy();
   
-PubKey getPublicKey(const std::string& password, const Factor2& factor,
-        const std::string& area);
-  
-  
-KeyPair getPublicKey(const std::string& password, const Factor2& factor,
-        const std::string& area);
+// PubKey getPublicKey(const std::string& password, const Factor2& factor,
+//         const std::string& area);
 
 uint64_t proveIdentity(const std::string& password, uint64_t nonce,
         const std::string& area);
@@ -80,3 +73,12 @@ void getUserid(const std::string& area, const std::string& name,
   
 // TODO: Require hardware authentication (à la YubiKey)?
 void recover2ndFactor();
+
+int main()
+{
+    generateKey("my.stevens.edu");
+    generateKey("google.com");
+    for (auto i : siteKeys)
+        std::cout << i.first << " " << i.second << std::endl;
+    return 0;
+}
