6f52353faec67051a1c17bbd66947eb164f62e1e
Tyler Kaminski <durcor@disroot.org>
2021-10-24 16:24:52 -0400

    Shell curl prototype fully-functioning

--- src/repo/examples/cloud/upload.sh
+++ src/repo/examples/cloud/upload.sh
@@ -1,33 +1,57 @@
 #!/bin/sh
 # https://towardsdatascience.com/uploading-files-to-google-drive-directly-from-the-terminal-using-curl-2b89db28bb06
 
-# File we want to upload (In our case, the website key/sitename pair file)
-FILENAME=
+setup()
+{
+    # Step 1. Visit https://console.developers.google.com/apis/credentials?pli=1
+    # Creating account will provide the client ID and client secret.
 
-# Step 1. Visit https://console.developers.google.com/apis/credentials?pli=1
-# Creating account will provide these tokens.
-CLIENT_ID=
-CLIENT_SECRET=
+    # Step 2. Verify device
+    codes=$(curl -d \
+        "client_id=$GOOGLE_CLIENT_ID&scope=https://www.googleapis.com/auth/drive.file" \
+        https://oauth2.googleapis.com/device/code | head -3 | cut -d'"' -f4)
+    DEVICE_CODE=$(echo "$codes" | head -2 | tail -1)
+    USER_CODE=$(echo "$codes" | tail -1)
 
-# Step 2. Verify device
-codes=$(curl -d \
-    "client_id=$CLIENT_ID&scope=https://www.googleapis.com/auth/drive.file" \
-    https://oauth2.googleapis.com/device/code | head -2 | cut -d'"' -f4)
-DEVICE_CODE=$(echo $codes | head -1)
-USER_CODE=$(echo $codes | tail -1)
+    xdg-open "https://www.google.com/device"
+    echo "Provide this code to the Google Device page that just was opened:"
+    echo "$USER_CODE"
+    echo ""
+    echo "Once you're done, enter 'y' to continue."
+    while [ "$continue_on" != 'y' ]; do
+        read -r continue_on
+    done
 
-# Step 3. Visit https://www.google.com/device and provide $USER_CODE
+    # Step 4: Bearer code: Used to fingerprint account.
+    curl -d client_id="$GOOGLE_CLIENT_ID" -d client_secret="$GOOGLE_CLIENT_SECRET" \
+        -d device_code="$DEVICE_CODE" \
+        -d grant_type=urn%3Aietf%3Aparams%3Aoauth%3Agrant-type%3Adevice_code \
+        https://accounts.google.com/o/oauth2/token
+}
 
-# Step 4: Bearer code: Used to fingerprint account.
-curl -d client_id="$CLIENT_ID" -d client_secret="$CLIENT_SECRET" \
-    -d device_code="$DEVICE_CODE" \
-    -d grant_type=urn%3Aietf%3Aparams%3Aoauth%3Agrant-type%3Adevice_code \
-    https://accounts.google.com/o/oauth2/token
-ACCESS_TOKEN=
+upload()
+{
+    # File we want to upload (In our case, the website key/sitename pair file)
+    FILENAME="$1"
+    # Refresh the access token each upload so you never get locked out.
+    ACCESS_TOKEN=$(curl -d client_id="$GOOGLE_CLIENT_ID" \
+        -d client_secret="$GOOGLE_CLIENT_SECRET" \
+        -d refresh_token="$GOOGLE_REFRESH_TOKEN" \
+        -d grant_type=refresh_token \
+        https://accounts.google.com/o/oauth2/token | head -2 | cut -d'"' -f4 | tail -1)
 
-# Step 5: Upload file
-curl -X POST -L \
-    -H "Authorization: Bearer $ACCESS_TOKEN" \
-    -F "metadata={name :'$FILENAME'};type=application/json;charset=UTF-8" \
-    -F "file=@$FILENAME;type=$(file --mime-type -b "$FILENAME")" \
-    "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart"
+    UPLOAD_ID=$(curl -X POST -L \
+        -H "Authorization: Bearer $ACCESS_TOKEN" \
+        -F "metadata={name :'$FILENAME'};type=application/json;charset=UTF-8" \
+        -F "file=@$FILENAME;type=$(file --mime-type -b "$FILENAME")" \
+        "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart" | head -3 | cut -d'"' -f4 | tail -1)
+
+    echo "File uploaded to:"
+    echo "https://drive.google.com/file/d/$UPLOAD_ID"
+}
+
+if [ ! "$1" ] || [ ! -f "$1" ]; then
+    setup
+else
+    upload "$1"
+fi
