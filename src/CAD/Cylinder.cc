1a50284dd38d4777300f07f0df705d7095e5ddae
ajohnso4 <ajohnso4@stevens.edu>
2021-11-16 17:42:26 -0500

    Update to cylinder

--- src/CAD/Cylinder.cc
+++ src/CAD/Cylinder.cc
@@ -1,11 +1,16 @@
 #include "CAD/Cylinder.hh"
 #include "opengl/GLMath.hh"
+#include "CAD/Vec3d.hh"
 #include <math.h>
+#include "glad/glad.h"
+#include "opengl/Shader.hh"
+#include "opengl/GLWin.hh"
+#include "opengl/Canvas.hh"
 
 
 Cylinder::Cylinder(uint32_t height, uint32_t r1, uint32_t r2, uint32_t res) : height(height), r1(r1), r2(r2), res(res){
 }
-Cylinder::Cylinder(uint32_t height, uint32_t r1, uint32_t res) : height(height), r1(r1), r2(r1), res(res){
+Cylinder::Cylinder(uint32_t height, uint32_t r1, uint32_t res) : height(height), r1(r1), res(res){
 }
 Cylinder::~Cylinder(){
 }
@@ -13,32 +18,29 @@ Cylinder::~Cylinder(){
 void Cylinder::init(){
   double chord = 2 * r1 * sin(PI<double>/res);
   rMin = Math.sqrt(r1 * r1 - chord * chord /4);
-  vector<vector<double>> topVerts;//Vector of vectors
-  vector<double> topVertex;
-  topVertex.push_back(0);
-  topVertex.push_back(0);
-  topVertex.push_back(height);
+  vector<Vec3D> topVerts;//Vector of vectors
+  Vec3D topVertex(0, 0, height);
   topVerts.push_back(topVertex);
-  vector<doubld> bottomVertex;
-  bottomVertex.push_back(0);
-  bottomVertex.push_back(0);
-  bottomVertex.push_back(0);
+  Vec3D bottomVertex(0, 0, 0);
   topVerts.push_back(bottomVertex);
   for(double theta = 0; theta < 2 * PI<double> - (1.0e-7); theta+=2 * PI<double> / res){
     double x = r1 * cos(theta);
     double y = r1 * sin(theta);
-    vector<double> firstVertex;
-    firstVertex.push_back(x);
-    firstVertex.push_back(y);
-    firstVertex.push_back(height);
+    Vec3D firstVertex(x, y, height);
     topVerts.push_back(firstVertex);
-    vector<double> secondVertex;
-    secondVertex.push_back(x);
-    secondVertex.push_back(y);
-    secondVertex.push_back(0);
+    Vec3D secondVertex(x, y, 0);
     topVerts.push_back(secondVertex);
   }
-  
+  //add to a ComplexPoly?
+  //Change to glGenBuffer and other grail functions?
+  //following code comes from triangle, but modified to how I think it might need to be for cylinder
+  //probably incorrect
+  // glGenVertexArrays(3, &vao);
+  // glBindVertexArray(vao);
+  // glGenBuffers(3, &vbo);
+  // glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  // glBufferData(GL_ARRAY_BUFFER, 3 * sizeof(uint32_t), &r1, GL_DYNAMIC_DRAW);
+  // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 2 * sizeof(uint32_t), (void*)0);
 }
 
 double Cylinder::getVolume(){
@@ -47,6 +49,21 @@ double Cylinder::getVolume(){
 }
 
 void Cylinder::render(){
+  Shader * shader = Shader::useShader(GLWin::COMMON_SHADER);
+  shader->setMat4("projection",*(parentCanvas->getProjection()));
+	shader->setVec4("solidColor",style->getFgColor());
+
+  glBindVertexArray(vao);
+  glEnableVertexAttributeArray(0);
+
+  glLineWidth(style->getLineWidth());
+  //Use triangle_strip, but not sure how many points would actually be necessary 
+  // glDrawArrays(GL_TRIANGLE_STRIP, 0, what I imagine is some large number)
+  // For circles of cylinder, one point for center, need more for whole circle
+  // glDrawArrays(GL_TRIANGLE_FAN, 0, 1)
+  glDisableVertexAttribArray(0);
+  glBindVertexArray(0);
+
 }
 
 void Cylinder::cleanup(){
