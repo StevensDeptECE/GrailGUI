2a7e33b75d9cc323139652501a61b57e190b0ffa
rtrshm <ashum@stevens.edu>
2021-11-18 14:14:31 -0500

    updated test file to comply with compiler warnings

--- src/repo/aes256/test_aes_enc_dec.cc
+++ src/repo/aes256/test_aes_enc_dec.cc
@@ -15,22 +15,22 @@ int main (void) {
 
     // put a test string into the file
     const char *test_string = "This is our test string!";
-    const int test_len = strlen(test_string);
+    const size_t test_len = strlen(test_string);
     test_file.write(test_string, test_len);
     test_file.close();
 
     // encrypt the file
-    int enc_result = cipher.encrypt_file("test.txt", "test.enc");
-    printf("Encrypted %d bytes.\n", enc_result);
+    long int enc_result = cipher.encrypt_file("test.txt", "test.enc");
+    printf("Encrypted %ld bytes.\n", enc_result);
     
     // decrypt the file 
-    int dec_result = cipher.decrypt_file("test.enc", "test.dec");
-    printf("Decrypted %d bytes.\n", dec_result);
+    long int dec_result = cipher.decrypt_file("test.enc", "test.dec");
+    printf("Decrypted %ld bytes.\n", dec_result);
 
     // check output contents
     std::ifstream result;
     result.open("test.dec", std::ios::in | std::ios::binary);
-    char in[test_len + 1];
+    char *in = new char[test_len + 1];
     result.read(in, test_len);
     in[test_len] = '\0';
     printf("Plaintext: %s\n"
