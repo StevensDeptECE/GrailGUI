d1023a6e59bcda28f749fc9bc7dbb6c5925e9061
Tyler Kaminski <durcor@disroot.org>
2021-12-01 14:32:00 -0500

    Move image backend to imagemagick, add Dov's prototype stegano algo

--- src/repo/stegano/steganography.cc
+++ src/repo/stegano/steganography.cc
@@ -1,52 +1,60 @@
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
 #include <iostream>
+#include <cstring>
+
+// https://imagemagick.org/Magick++/tutorial/Magick++_tutorial.pdf
+#include <Magick++.h>
 
 class SteganographicImage {
 private:
-    int w = 0, h = 0, c = 0;
+    int w, h, c;
+    Magick::Image img;
+    Magick::PixelPacket *pix;
     char *filename;
     uint32_t start = 0; // start of the data (x=10,y=13) 13*w+h
-    uint32_t stride = 2; // how many to skip =2
+    uint32_t stride = 5; // how many to skip
     // uint32_t bits_per_color = 1;
-    uint8_t *data;
+    uint64_t *data;
     uint32_t len;
 
 public:
-    unsigned char *rgb;
-    SteganographicImage(char *filename, uint8_t *data, uint32_t len) :
+    SteganographicImage(char *filename, uint64_t *data, uint32_t len) :
         filename(filename), data(data), len(len) {
-            rgb = stbi_load(filename, &w, &h, &c, 0);
+            img.read(filename);
+            h = img.rows(),
+            w = img.columns();
+            c = img.channels();
+            pix = img.getPixels(0, 0, h, w);
         }
 
-    ~SteganographicImage() {
-        stbi_image_free(rgb);
-    }
+    ~SteganographicImage() { }
 
     void hide() {
-        for (uint32_t i = start; i < len * stride; i += stride) {
-            auto id = (start + i) / stride;
-            if (!data[id])
-                rgb[i] &= data[id] + 0b11111110;
-            else
-                rgb[i] |= data[id];
-        }
+        for (uint32_t i = 0; i < h; ++i)
+            for (uint32_t j = 0; j < w; ++j)
+                for (uint32_t k = 0; k < len; ++k) {
+                    uint32_t pos = start + k * stride;
+                    uint64_t v = data[k]; // v is what you put into your image
+                    pix[pos] &= 0b111111101111111011111110; // wipe out the low bit of each r,g,b
+                    pix[pos] |= v & 1; // get each rightmost bit and put 1 bit into low part of blue
+                    pix[pos] |= ((v >> 1) & 1) << 8; // or in the low green bit
+                    pix[pos] |= ((v >> 2) & 1) << 16;
+                }
+        img.syncPixels();
     }
 
     uint32_t* recover() {
         uint32_t *recovered = new uint32_t[len];
-        for (int i = start; i < len * stride; i += stride)
-            recovered[(start + i) / stride] = rgb[i] & 1;
+        for (uint32_t i = 0; i < len; ++i) {
+            uint32_t pos = start + i * stride;
+            recovered[i] = pix[i] & 1;
+        }
         return recovered;
     }
 
     void write() {
-        stbi_write_jpg(filename, w, h, c, rgb, w * c);
+        img.write(filename);
         std::cout << "File written to " << filename << std::endl;
     }
-
 };
 
 int main(int argc, char **argv)
@@ -57,29 +65,25 @@ int main(int argc, char **argv)
     }
     char *filename = argv[1];
     // Data to hide in message and length of message.
-    // Test 1: Binary string.
-    uint8_t data[] = {0, 1, 0, 1, 0, 1, 0, 1, 0, 1};
-    int n = 10;
+    char *msg = (char *)"hello";
+    int n = strlen(msg);
+    uint64_t *data = new uint64_t[n];
+    for (int i = 0; i < n; ++i)
+        data[i] = (int) msg[i];
 
-    // Test 2: ASCII text.
-    // char *msg = (char *)"hello";
-    // int n = strlen(msg);
-    // uint8_t *data = new uint8_t[n];
-    // for (int i = 0; i < n; ++i)
-    //     data[i] = (int) msg[i];
-
-    SteganographicImage steg = SteganographicImage(filename, data, n);
-    if (!steg.rgb) {
-        std::cerr << "Error: " << filename << " does not exist." << std::endl;
+    try {
+        SteganographicImage steg = SteganographicImage(filename, data, n);
+        steg.hide();
+        // Write a new image storing the secret message.
+        steg.write();
+        // Recover the secret message from the picture and print it out.
+        auto recovered = steg.recover();
+        for (int i = 0; i < n; ++i)
+            std::cout << (char) recovered[i] << " ";
+        std::cout << std::endl;
+    } catch (std::exception &e) {
+        std::cerr << "Error: " << e.what() << std::endl;
         return 1;
     }
-    steg.hide();
-    // Write a new image storing the secret message.
-    steg.write();
-    // Recover the secret message from the picture and print it out.
-    auto *recovered = steg.recover();
-    for (int i = 0; i < n; ++i)
-        std::cout << recovered[i] << " ";
-    std::cout << std::endl;
     return 0;
 }
