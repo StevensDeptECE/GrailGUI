2abe0822993d9a52fc7e7c807981a23b4b831fba
ajohnso4 <ajohnso4@stevens.edu>
2021-12-08 09:58:00 -0500

    cylinder changes

--- src/CAD/Cylinder.cc
+++ src/CAD/Cylinder.cc
@@ -1,51 +1,58 @@
 #include "CAD/Cylinder.hh"
 #include "opengl/GLMath.hh"
 #include "CAD/Vec3d.hh"
-#include <math.h>
+#include <cmath>
+#include <numbers>
 #include "glad/glad.h"
 #include "opengl/Shader.hh"
 #include "opengl/GLWin.hh"
 #include "opengl/Canvas.hh"
 
+using namespace std::numbers;
 
 Cylinder::Cylinder(uint32_t height, uint32_t r1, uint32_t r2, uint32_t res) : height(height), r1(r1), r2(r2), res(res){
 }
-Cylinder::Cylinder(uint32_t height, uint32_t r1, uint32_t res) : height(height), r1(r1), res(res){
+Cylinder::Cylinder(uint32_t height, float x, float y,float radius, uint32_t segments, const Style* s) : height(height), radius(radius), res(res), x(x), y(y), segments(segments){
 }
 Cylinder::~Cylinder(){
 }
 
 void Cylinder::init(){
-  double chord = 2 * r1 * sin(PI<double>/res);
-  rMin = Math.sqrt(r1 * r1 - chord * chord /4);
-  vector<Vec3D> topVerts;//Vector of vectors
-  Vec3D topVertex(0, 0, height);
-  topVerts.push_back(topVertex);
-  Vec3D bottomVertex(0, 0, 0);
-  topVerts.push_back(bottomVertex);
-  for(double theta = 0; theta < 2 * PI<double> - (1.0e-7); theta+=2 * PI<double> / res){
-    double x = r1 * cos(theta);
-    double y = r1 * sin(theta);
-    Vec3D firstVertex(x, y, height);
-    topVerts.push_back(firstVertex);
-    Vec3D secondVertex(x, y, 0);
-    topVerts.push_back(secondVertex);
-  }
-  //add to a ComplexPoly?
-  //Change to glGenBuffer and other grail functions?
-  //following code comes from triangle, but modified to how I think it might need to be for cylinder
-  //probably incorrect
-  // glGenVertexArrays(3, &vao);
-  // glBindVertexArray(vao);
-  // glGenBuffers(3, &vbo);
-  // glBindBuffer(GL_ARRAY_BUFFER, vbo);
-  // glBufferData(GL_ARRAY_BUFFER, 3 * sizeof(uint32_t), &r1, GL_DYNAMIC_DRAW);
-  // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 2 * sizeof(uint32_t), (void*)0);
-}
+  glGenVertexArrays(1, &vao);
+  glBindVertexArray(vao);
 
-double Cylinder::getVolume(){
-  double volume = PI<double> * r1 * r2 * height;
-  return volume;
+  uint32_t vertices = segments + 2;
+  
+
+
+  for(int i=0; i < vertices; i++){
+    vert.push_back(x +  (radius * cos( i * (2 * pi / segments))));
+    vert.push_back(y +  (radius * sin( i * (2 * pi / segments))));
+    vert.push_back(0); //z=0
+  }
+  for(int i=0; i < vertices; i++){
+    vert.push_back(x +  (radius * cos( i * (2 * pi / segments))));
+    vert.push_back(y + (radius * sin( i * (2 * pi / segments))));
+    vert.push_back(height); //z=height
+  }
+
+
+  for(int i=0; i < vertices/2; i++){
+    ind.push_back(i);
+    ind.push_back(vertices + i);
+    ind.push_back(i+1);
+    ind.push_back(i);
+    ind.push_back(i+1);
+    ind.push_back(vertices + i + 1);
+  }
+
+  glGenBuffers(1, &vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, vertices * sizeof(float), &allVerts, GL_DYNAMIC_DRAW);
+  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+  glGenBuffers(1, &ibo);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, ind.size() * sizeof(uint32_t), &ind[0], GL_STATIC_DRAW);
 }
 
 void Cylinder::render(){
@@ -56,15 +63,22 @@ void Cylinder::render(){
   glBindVertexArray(vao);
   glEnableVertexAttributeArray(0);
 
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementbuffer);
+
   glLineWidth(style->getLineWidth());
-  //Use triangle_strip, but not sure how many points would actually be necessary 
-  // glDrawArrays(GL_TRIANGLE_STRIP, 0, what I imagine is some large number)
-  // For circles of cylinder, one point for center, need more for whole circle
-  // glDrawArrays(GL_TRIANGLE_FAN, 0, 1)
+
+  glDrawArrays(GL_TRIANGLE_FAN, 0, segments + 2);
+  glDrawElements(GL_TRIANGLES, ind.size(), GL_UNSIGNED_INT, (void*)0);
   glDisableVertexAttribArray(0);
   glBindVertexArray(0);
 
 }
+
+
+// double Cylinder::getVolume(){
+//   double volume = PI<double> * r1 * r2 * height;
+//   return volume;
+// }
 
 void Cylinder::cleanup(){
 }
--- src/CAD/Cylinder.hh
+++ src/CAD/Cylinder.hh
@@ -7,7 +7,7 @@ class Cylinder : public Shape{
         std::vector<uint32_t> ind;
     public:
         Cylinder(uint32_t height, uint32_t r1, uint32_t r2);
-        Cylinder(uint32_t height, uint32_t r1);
+        Cylinder::Cylinder(uint32_t height, float x, float y,float radius, uint32_t segments, const Style* s)
         ~Cylinder();
         double getVolume();
         void init();
