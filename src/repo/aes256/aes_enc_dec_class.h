32181661304fa16387ef3dc6acd8ad80a655445f
rtrshm <ashum@stevens.edu>
2021-12-08 16:48:13 -0500

    added a nauseating amount of error checking

--- src/repo/aes256/aes_enc_dec_class.h
+++ src/repo/aes256/aes_enc_dec_class.h
@@ -13,14 +13,19 @@ void handleErrors() {
     abort();
 }
 
+void (std::ifstream *in, std::ofstream *out) {
+    in->close();
+    out->close();
+}
+
 /**
  * @brief An instance of AESEncDec is tied to a key, with which
  * files can be encrypted or decrypted as desired.
  */
 class AESEncDec {
-        static const int KEYLEN = 32;
-        static const int BLOCKSIZE = 16;
-        static const int ITER_COUNT = 10000;
+        static constexpr int KEYLEN = 32;
+        static constexpr int BLOCKSIZE = 16;
+        static constexpr int ITER_COUNT = 10000;
         unsigned char key[KEYLEN];
         unsigned char iv[BLOCKSIZE];
     public:
@@ -60,18 +65,25 @@ int AESEncDec::encrypt_file(const char* path, const char* out) {
     
     // initialize encryption context
     EVP_CIPHER_CTX *ctx;
-    if (!(ctx = EVP_CIPHER_CTX_new()))
-        handleErrors();
+    if (!(ctx = EVP_CIPHER_CTX_new())) {
+        std::cerr << "Failed to initialize cipher context";
+        close_files(&plaintext_file, &ciphertext_file);
+        return -1;
+    }
 
     // reinitialize iv to avoid reuse
     if (!RAND_bytes(iv, BLOCKSIZE)) {
-        fprintf(stderr, "Failed to initialize IV");
+        std::cerr << "Failed to initialize IV";
+        close_files(&plaintext_file, &ciphertext_file);
         return -1;
     }
 
     // set cipher/key/iv
-    if (1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), nullptr, key, iv))     
-        handleErrors();
+    if (1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), nullptr, key, iv)) {
+        std::cerr << "Failed to initialize encrypt";
+        close_files(&plaintext_file, &ciphertext_file);
+        return -1;
+    } 
 
     // for keeping track of result length
     int len;
@@ -82,25 +94,48 @@ int AESEncDec::encrypt_file(const char* path, const char* out) {
     while (1) {
         plaintext_file.read((char *) plaintext, BLOCKSIZE);
         bytes_read = plaintext_file.gcount();
-        if (1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, bytes_read))
-            handleErrors();
-        
+        if (1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, bytes_read)) {
+            std::cerr << "Failed to continue encryption";
+            close_files(&plaintext_file, &ciphertext_file);
+            return -1;
+        }
+
         ciphertext_file.write((char *) ciphertext, len);
+        if (!ciphertext_file.good()) {
+            std::cerr << "Failed to write to cipher";
+            close_files(&plaintext_file, &ciphertext_file);
+            return -1;
+        }
+
         cipherlen+=len;
         if (bytes_read < BLOCKSIZE) break;
     }
 
     // finalize encryption
-    if (1 != EVP_EncryptFinal_ex(ctx, ciphertext, &len))
-        handleErrors();
+    if (1 != EVP_EncryptFinal_ex(ctx, ciphertext, &len)) {
+        std::cerr << "Failed to finalize encrypt";
+        close_files(&plaintext_file, &ciphertext_file);
+        return -1;
+    }
+
+    if (!plaintext_file.eof()) {
+        std::cerr << "Failed to read entire plaintext";
+        close_files(&plaintext_file, &ciphertext_file);
+        return -1;
+    }
 
     // write final block
     ciphertext_file.write((char *) ciphertext, len);
+    if (!ciphertext_file.good()) {
+        std::cerr << "Failed to write to cipher";
+        close_files(&plaintext_file, &ciphertext_file);
+        return -1;
+    }
+
     cipherlen += bytes_read;
 
     // clean up
-    ciphertext_file.close();
-    plaintext_file.close();
+    close_files(&plaintext_file, &ciphertext_file);
     EVP_CIPHER_CTX_free(ctx);
     return cipherlen;
 } 
@@ -116,19 +151,26 @@ int AESEncDec::decrypt_file(const char* path,
 
     // if opening failed, exit
     if (!ciphertext_file.is_open() || !plaintext_file.is_open()) {
-        fprintf(stderr, "One of the files is already open.\n");
+        std::cerr << "One of the files is already open";
+        close_files(&ciphertext_file, &plaintext_file);
         return -1;
     }
     
     // initialize cipher context
     EVP_CIPHER_CTX *ctx;
-    if (!(ctx = EVP_CIPHER_CTX_new()))
-        handleErrors();
+    if (!(ctx = EVP_CIPHER_CTX_new())) {
+        std::cerr << "Failed to initialize cipher context";
+        close_files(&ciphertext_file, &plaintext_file);
+        return -1;
+    }
 
     // TODO:: Attach IV to the file 
 
-    if (1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), nullptr, key, iv))
-        handleErrors();
+    if (1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), nullptr, key, iv)) {
+        std::cerr << "Failed to initialize decrypt";
+        close_files(&ciphertext_file, &plaintext_file);
+        return -1;
+    }
     
     // keeping track of length of result
     int len;
@@ -145,23 +187,51 @@ int AESEncDec::decrypt_file(const char* path,
         bytes_read = ciphertext_file.gcount();
 
         // decrypt block
-        if (1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, bytes_read))
-            handleErrors();
+        if (1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, bytes_read)) {
+            std::cerr << "Decrypt failed";
+            close_files(&ciphertext_file, &plaintext_file);
+            // Proper way to exit??
+            return -1;
+        }
         plaintext_len += len;
+        
+        // write decrypted result and ensure success
         plaintext_file.write((char *) plaintext, len);
+        if (!plaintext_file.good()) {
+            std::cerr << "Failed to write decryption";
+            close_files(&ciphertext_file, &plaintext_file);
+            return -1;
+        }
         if (bytes_read < BLOCKSIZE) break;        
     } 
 
+    // consider working on copies of file to avoid damaging user's content?
     if (1 != EVP_DecryptFinal_ex(ctx, plaintext + len, &len))
-        handleErrors();
+    {
+        std::cerr << "EVP_DecryptFinal failed";
+        close_files(&ciphertext_file, &plaintext_file);
+        return -1;
+    }
 
+    if (!ciphertext_file.eof()) {
+        std::cerr << "Reading from ciphertext failed";
+        close_files(&ciphertext_file, &plaintext_file);
+        return -1;
+    }
+    
+    // write & ensure success
     plaintext_file.write((char*) plaintext, len);
+    if (!plaintext_file.good()) {
+        std::cerr << "Writing to plaintext failed.";
+        close_files(&ciphertext_file, &plaintext_file);
+        return -1;
+    }
+
     plaintext_len += len;
     
     // clean up
     EVP_CIPHER_CTX_free(ctx);
-    plaintext_file.close();
-    ciphertext_file.close();
+    close_files(&ciphertext_file, &plaintext_file);
 
     return plaintext_len;
 }
