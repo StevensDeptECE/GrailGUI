31f7d4fc877f4445ab06c1d6d5e57b83e7a93ef9
ahuston-0 <aliceghuston@gmail.com>
2021-12-19 11:20:27 -0500

    Turn SocketIO into a namespace
    Previously, SocketIO was a class containing static functions and
    constants. In a situation like this, it makes a bit more sense to use a
    namespace (eventually if at some point we want to allow argument
    dependent lookup for a function).

--- src/csp/SocketIO.cc
+++ src/csp/SocketIO.cc
@@ -6,22 +6,22 @@
 
 // TODO: Look into logging WSAGetLastError and strerror(errno)
 // Using the logging object in csp.hh?
-int SocketIO::send(socket_t sckt, const char *buf, int size, int flags) {
+namespace SocketIO {
+int send(socket_t sckt, const char *buf, int size, int flags) {
   uint32_t bytesSent;
   if ((bytesSent = ::send(sckt, (char *)buf, size, 0)) == err_code) {
-    if (errno != EBADF)
-      throw Ex1(Errcode::SOCKET_SEND);
+    if (errno != EBADF) throw Ex1(Errcode::SOCKET_SEND);
     perror("Warning on send(): ");
   }
   return bytesSent;
 }
 
-int SocketIO::recv(socket_t sckt, const char *buf, int size, int flags) {
+int recv(socket_t sckt, const char *buf, int size, int flags) {
   uint32_t bytesRecv;
   if ((bytesRecv = ::recv(sckt, (char *)buf, size, 0)) == err_code) {
-    if (errno != EBADF)
-      throw Ex1(Errcode::SOCKET_SEND);
+    if (errno != EBADF) throw Ex1(Errcode::SOCKET_SEND);
     perror("Warning on recv()");
   }
   return bytesRecv;
 }
+}  // namespace SocketIO
