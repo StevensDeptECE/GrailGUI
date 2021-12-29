0811a7a013a4010cb329d89890f74c020f6ab3e2
kaiqichee <55299653+kaiqichee@users.noreply.github.com>
2021-12-29 12:43:09 -0800

    compiled bezier
    start point seems off, but compiles and runs

--- src/CAD/Vec3d.hh
+++ src/CAD/Vec3d.hh
@@ -1,7 +1,10 @@
 #pragma once
 #include <iostream>
 #include <cmath>
+
 using namespace std;
+
+
 // create constructor to create the vec_3d objects
 class Vec3D{
 public:
@@ -41,16 +44,16 @@ double dot(const Vec3D& a, const Vec3D& b){
 return a.x * b.x + a.y * b.y + a.z * b.z;
 }
 // Overload the << operator
-ostream& operator<< (ostream& s, const Vec3D& v){
+std::ostream& operator<< (std::ostream& s, const Vec3D& v){
 return s << "(" << v.x <<"," << v.y << "," << v.z <<")";
 }
 // Overload the >> operator
-istream& operator>> (istream& s, Vec3D& v){
+std::istream& operator>> (std::istream& s, Vec3D& v){
 return s >> v.x >> v.y >> v.z;
 }
 // int main() {
 // const Vec3D a(1,2,3);
 // const Vec3D b(1,2,3);
 // cout << a.magsq(); 
-// cout << a << b
+// cout << a << b;
 // }
--- src/CAD/bezier.hh
+++ src/CAD/bezier.hh
@@ -1,20 +1,39 @@
 #pragma once
-#include "opengl/GrailGUI.hh"
-#include "src/CAD/Vec3d.hh"
-#include "src/CAD/Curve.hh"
+//#include "opengl/GrailGUI.hh"
+#include "./Vec3d.hh"
+#include "./Curve.hh"
+#include <vector>
+
 //#include "src/CAD/line.hh"
 
 
-class Bezier extends Curve {
+class Bezier : public Curve {
  protected:
-    std::vector<Curve> segments;
+    //std::vector<Curve> segments;
+    std::vector<Vec3D> point;
+    float ax, bx, cx, dx;
+    float ay, by, cy, dy;
+    float az, bz, cz, dz;
   
   public:
-    Bezier() {
+    Bezier(Vec3D p1, Vec3D p2, Vec3D p3, Vec3D p4) {
+      point.push_back(p1);
+      point.push_back(p2);
+      point.push_back(p3);
+      point.push_back(p4);
     }
 
-  Vec3D getPoint(double step);
-  Vec3D getTangent(double step);
+  void getEquationX(const Vec3D& p0, const Vec3D& p1, const Vec3D& p2, const Vec3D& p3);
+  void getEquationY(const Vec3D& p0, const Vec3D& p1, const Vec3D& p2, const Vec3D& p3);
+  void getEquationZ(const Vec3D& p0, const Vec3D& p1, const Vec3D& p2, const Vec3D& p3);
+  float x(float t) const;
+  float y(float t) const;
+  float z(float t) const;
+
+  
+
+  Vec3D getPoint(double step) override {return Vec3D(0,0,0);}
+  Vec3D getTangent(double step) override {return Vec3D(0,0,0);}
   //Matrix3D getMatrix(double step);
 
-}
+};
