2c16c38dcd523edffaf4417bd929a24270ee5efc
ajohnso4 <ajohnso4@stevens.edu>
2022-02-02 16:48:00 -0500

    sphere update

--- src/CAD/Sphere.cc
+++ src/CAD/Sphere.cc
@@ -8,16 +8,71 @@
 #include "opengl/GLWin.hh"
 #include "opengl/Canvas.hh"
 
-Sphere::Sphere(uint32_t radius, uint32_t x, uint32_t y, uint32_t z) : radius(radius), x(x), y(y), z(z){
+Sphere::Sphere(uint32_t latRes, uint32_t longRes, uint32_t radius) : latRes(latRes), longRes(longRes), radius(radius){
 }
 Sphere::~Sphere(){
 }
 
 void Sphere::init(){
-  
+  //create vertex array object
+  glGenVertexArrays(1, &vao);
+  glBindVertexArray(vao);
+
+  const double dphi = pi / latRes;
+  double phi = -pi / 2 + dphi;
+
+  //band around sphere
+  for (int i = 0; i < latRes - 1; ++i, phi += dphi){
+    double z = radius + sin(phi);
+    double r2 = radius * cos(phi);
+    double theta = 0;
+    const double dtheta = 2 * pi / longRes;
+    
+    for(int j = 0; j < longRes; ++j, theta += dtheta){
+      double x = r2 * cos(theta), y = r2 * sin(theta);
+      vert.push_back(x);
+      vert.push_back(y); 
+      vert.push_back(z);
+    }
+  }
+
+  //rectangles
+  for (int i = 0; i < vert.size() - longRes; i++){
+    ind.push_back(i + longRes);
+    ind.push_back(i);
+    ind.push_back((i % longRes == longRes - 1 ? -longRes : 0) + i + 1);
+    ind.push_back(i + longRes);
+    ind.push_back((i % longRes == longRes - 1 ? -longRes : 0) + i + 1); 
+    ind.push_back((i % longRes == longRes - 1 ? -longRes : 0) + i + longRes + 1);
+  }
+
+  //north and south
+  vert.push_back(0); vert.push_back(0); vert.push_back(-radius);
+  vert.push_back(0); vert.push_back(0); vert.push_back(radius);
+
+  //Vertices
+  glGenBuffers(1, &vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vert.size(), &vert[0], GL_STATIC_DRAW);
+
+  //Indices
+  glGenBuffers(1, &lbo);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, lbo);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(uint32_t)*ind.size(), &ind[0], GL_STATIC_DRAW);
+
+
 }
 
 void Sphere::render(){
+  
+  glBindVertexArray(vao);
+  glEnableVertexAttribArray(0);
+  
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, lbo);
+  glDrawElements(GL_LINES, ind.size(), uint32_t, 0);
+
+  glDisableVertexAttribArray(0);
+  glBindVertexArray(0);
 }
 
 void Sphere::cleanup(){
--- src/CAD/Sphere.hh
+++ src/CAD/Sphere.hh
@@ -5,9 +5,9 @@ class Sphere {
     private:
         std::vector<float> vert;
         std::vector<uint32_t> ind;
-        uint32_t radius, x, y, z;
+        uint32_t latRes, longRes, radius;
     public:
-        Sphere::Sphere(uint32_t radius, uint32_t x, uint32_t y, uint32_t z);
+        Sphere::Sphere(uint32_t latRes, uint32_t longRes, uint32_t radius);
         ~Sphere();
         void init();
         void render();
