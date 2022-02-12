02cdb3bfa7c9011dc0831678237687fa27da2615
Tyler Kaminski <durcor@disroot.org>
2022-02-12 15:47:38 -0500

    Add algo for hiding and recovering string in WebP

--- src/repo/stegano/decenc.cpp
+++ src/repo/stegano/decenc.cpp
@@ -5,7 +5,43 @@
 #include <fstream>
 #include <iostream>
 
-uint8_t *hideStr(uint8_t *data, char *str) { return 0; }
+int WEBP_HEADER_SIZE = 16;
+
+uint8_t *hide(uint8_t *data, size_t s, char *str) {
+  int bit = 0;
+  char c = *str++;
+  for (int i = WEBP_HEADER_SIZE; i < s && str; ++i) {
+    std::cout << (int)c << std::endl;
+    c >>= 7 - bit;
+    data[i] = c & 1 ? data[i] | 1 : data[i] & ~1;
+    ++bit;
+    if (bit == 8) {
+      bit = 0;
+      c = *str++;
+      if (!c) break;
+    }
+  }
+  return data;
+}
+
+char *recover(uint8_t *data, size_t s) {
+  int bit = 0;
+  char *str = new char[4096];
+  int n = 0;
+  char c = 0;
+  for (int i = WEBP_HEADER_SIZE; i < s; ++i) {
+    if (data[i] & 1) c |= 1;
+    ++bit;
+    if (bit == 8) {
+      if (c == '\0') break;
+      bit = 0;
+      str[n++] = c;
+    } else
+      c <<= 1;
+  }
+  str[n] = '\0';
+  return str;
+}
 
 int main(int argc, char **argv) {
   if (argc != 2) {
@@ -18,13 +54,13 @@ int main(int argc, char **argv) {
   // Get byte size of file and return for reading.
   fi.seekg(0, std::ios::end);
   size_t s = fi.tellg();
-  std::cout << "Number of bytes: " << s << std::endl;
+  // std::cout << "Number of bytes: " << s << std::endl;
   fi.clear();
   fi.seekg(0);
 
   uint8_t *data = new uint8_t[s];
   fi.read((char *)data, s);
-  for (int i = 0; i < 12; ++i) std::cout << (char)data[i] << " ";
+  // for (int i = 0; i < 36; ++i) std::cout << (char)data[i] << " ";
   std::cout << std::endl;
 
   int w, h;
@@ -32,13 +68,13 @@ int main(int argc, char **argv) {
     std::cerr << "Error: Input image is not a valid WebP file." << std::endl;
     return 1;
   }
-  std::cout << "width: " << w << std::endl;
-  std::cout << "height: " << h << std::endl;
+  // std::cout << "width: " << w << std::endl;
+  // std::cout << "height: " << h << std::endl;
 
   data = WebPDecodeRGB(data, s, &w, &h);
 
   char *str = (char *)"hello world";
-  hideStr(data, str);
+  data = hide(data, s, str);
 
   uint8_t *out;
   // NOTE: these functions, like the lossy versions, use the library's default
@@ -49,6 +85,11 @@ int main(int argc, char **argv) {
 
   std::ofstream fo("new_" + std::string(argv[1]), std::ios::binary);
   fo.write((char *)out, s);
+
+  char *recov = recover(data, s);
+  printf("%s\n", recov);
+  delete[] recov;
+
   WebPFree(data);
   WebPFree(out);
   return 0;
