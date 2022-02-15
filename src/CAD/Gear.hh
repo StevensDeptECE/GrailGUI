e156e675964302d9a53fe57abf3b130710fca11c
basileman <joepbasile@comcast,et>
2022-02-15 18:04:44 -0500

    gear update

--- src/CAD/Gear.hh
+++ src/CAD/Gear.hh
@@ -5,8 +5,8 @@
 class Gear : public Shape2D{
 private:
   float x, y, innerRadius, outterRadius, numTeeth, toothDepth, width;
-  uint32_t numOfverticies = 362;
-  float circleVerts[724];
+  
+  std::vector<float> verticies;
 public:
   Gear(Canvas* c, float x, float y, float innerradius, float outterradius, float numTeeth, float toothDepth, float width, const Style* s);
   void init();
