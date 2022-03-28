01a842495d21aa6c55eefb0a8ef828b6ef919598
rtrshm <ashum@stevens.edu>
2022-03-28 19:40:38 -0400

    template compiles, testing repo file added

---
+++ src/repo/template.cc
@@ -0,0 +1,83 @@
+#include <openssl/ssl.h>
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include "template.hh"
+#include "util/HashMap.hh"
+
+GrailRepository::GrailRepository(uint32_t size) : regions(64) {
+  std::vector<uint8_t> bytes(size, 0);
+}
+
+GrailRepository::~GrailRepository() {
+}
+
+void GrailRepository::generateKey(const std::string& siteName)
+{
+    int bits = 2048;
+
+    BIGNUM *bne = BN_new();
+    int ret = BN_set_word(bne, RSA_F4);
+    if (ret != 1)
+        std::cerr << "Error: Creating bignum failed." << std::endl;
+
+    RSA *r = RSA_new();
+    ret = RSA_generate_key_ex(r, bits, bne, NULL);
+    if (ret != 1)
+        std::cerr << "Error: Generating key pair failed." << std::endl;
+
+    // // XXX: Should we save keys to disk?
+    // BIO *bp_public = BIO_new_file("public.pem", "w+");
+    // ret = PEM_write_bio_RSAPublicKey(bp_public, r);
+    // if (ret != 1)
+    //     std::cerr << "Error: Writing public key failed." << std::endl;
+
+    // BIO *bp_private = BIO_new_file("private.pem", "w+");
+    // ret = PEM_write_bio_RSAPrivateKey(bp_private, r, NULL, NULL, 0, NULL, NULL);
+    // if (ret != 1)
+    //     std::cerr << "Error: Writing private key failed." << std::endl;
+
+    // Couple each site with its keypair.
+    siteKeys.push_back(std::make_pair(siteName, r));
+}
+
+void GrailRepository::deleteKey(const std::string& siteName)
+{
+    for (int i = 0; i < siteKeys.size(); ++i)
+        if (siteKeys[i].first == siteName) {
+            siteKeys.erase(siteKeys.begin() + i);
+            return;
+        }
+
+    std::cerr << "Error: Site: " << siteName << " not in site list."
+        << std::endl;
+}
+
+
+
+void GrailRepository::backupToCloud(const std::string& service_name, const std::string& userid,
+        const std::string& passwd, uint32_t offset, uint32_t stride,
+        uint32_t bits) {};
+
+void GrailRepository::restoreFromCloud(const std::string& service_name,
+        const std::string& userid, const std::string& passwd, uint32_t offset,
+        uint32_t stride, uint32_t bits) {};
+
+void unlock(const std::string& password, const Factor2& factor,
+        const std::string& area) {};
+
+void GrailRepository::scramble(const std::string& area) {};
+  
+void GrailRepository::destroy() {};
+  
+// PubKey getPublicKey(const std::string& password, const Factor2& factor, const std::string& area) 
+// }
+
+uint64_t GrailRepository::proveIdentity(const std::string& password, uint64_t nonce,
+        const std::string& area) {return 0;}
+  
+void GrailRepository::getUserid(const std::string& area, const std::string& name,
+        std::string& userid, std::string& passwd) {};
+  
+// TODO: Require hardware authentication (à la YubiKey)?
+void GrailRepository::recover2ndFactor() {};
--- src/repo/template.hh
+++ src/repo/template.hh
@@ -1,12 +1,20 @@
+#include "../util/HashMap.hh"
+#include <vector>
+#include <openssl/ssl.h>
+class PubKey {};
+class SymmetricKey {};
+class Factor2 {};
+class KeyPair {};
 class GrailRepository {
   private:
+    std::vector<std::pair<std::string, RSA *>> siteKeys;
     SymmetricKey key; // 2nd key to unlock this file
     // note: this example implementation shows only how to create one file,not the alternate versions in feature 4  
     struct Range { // specifies a section of the file for encryption
       uint32_t startOffset;
       uint32_t size;
     };
-    HashMap<Range region> regions;
+    HashMap<Range> regions;
     std::vector<uint8_t> bytes; // the raw bytes of the repository
   public:
     GrailRepository(uint32_t size); // set up empty repo with size bytes of storage
@@ -17,11 +25,11 @@ class GrailRepository {
     void generateKey(const std::string& siteName); // generate a new key for the specified website
     void deleteKey(const std::string& siteName); // unregister for site?
   
-    void backupToCloud(const std::string& service name,
+    void backupToCloud(const std::string& service_name,
       const std::string& userid, const std::string& passwd,
       uint32_t offset, uint32_t stride, uint32_t bits);
   
-    void restoryFromCloud(const std::string& service name,
+    void restoreFromCloud(const std::string& service_name,
       const std::string& userid, const std::string& passwd,
       uint32_t offset, uint32_t stride, uint32_t bits);
   
@@ -29,21 +37,15 @@ class GrailRepository {
     void unlock(const std::string& password, const Factor2& factor, const std::string& area); 
   
    // overwrite the array in RAM with random numbers to get rid of data in the clear
-    void scramble(const std::String& area);
-  
+    void scramble(const std::string& area);
   
   // scramble array with random values and write back to disk, destroying this repository.
     void destroy();
   
   // extract public key
-    PubKey getPublicKey(const std::string& password, const Factor2& factor, const std::string& area);
-  
-  
+    PubKey getPublicKey(const std::string& password, const Factor2& factor, const std::string& area);  
   // extract key pair so it can be used
-    KeyPair getPublicKey(const std::string& password, const Factor2& factor, const std::string& area);
-  
-  
-  
+    KeyPair getKeyPair(const std::string& password, const Factor2& factor, const std::string& area);
   
   // Given digital authentication challenge, take the given nonce, decrypt it, and send it back.
   // note that this could perhaps be used in an attack, so perhaps rather than giving anyone an answer to their challenge, send the answer back encrypted so that only the site claiming to be sending it can 
