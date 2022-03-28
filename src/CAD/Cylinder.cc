5e827d56c4ab52dc52d29e69d4a9a3dac466d067
ahuston-0 <aliceghuston@gmail.com>
2022-03-28 14:38:56 -0400

    fixed issues breaking compile

--- src/CAD/Cylinder.cc
+++ src/CAD/Cylinder.cc
@@ -1,24 +1,20 @@
 #include "CAD/Cylinder.hh"
+
 #include "opengl/GLMath.hh"
 
-Cylinder::Cylinder(uint32_t height, uint32_t r1, uint32_t r2) : height(height), r1(r1), r2(r2){
-}
-Cylinder::Cylinder(uint32_t height, uint32_t r1) : height(height), r1(r1), r2(r1){
-}
-Cylinder::~Cylinder(){
-}
+Cylinder::Cylinder(Canvas* parent, uint32_t height, uint32_t r1, uint32_t r2)
+    : Shape(parent), height(height), r1(r1), r2(r2) {}
+Cylinder::Cylinder(Canvas* parent, uint32_t height, uint32_t r1)
+    : Shape(parent), height(height), r1(r1), r2(r1) {}
+Cylinder::~Cylinder() {}
 
-void Cylinder::init(){
+void Cylinder::init() {}
 
-}
-
-double Cylinder::getVolume(){
+double Cylinder::getVolume() {
   double volume = PI<double> * r1 * r2 * height;
   return volume;
 }
 
-void Cylinder::render(){
-}
+void Cylinder::render() {}
 
-void Cylinder::cleanup(){
-}
+void Cylinder::cleanup() {}
--- src/CAD/RectangularPrism.hh
+++ src/CAD/RectangularPrism.hh
@@ -5,9 +5,10 @@ class RectangularPrism : public Shape{
     private:
         std::vector<float> vert;
         std::vector<uint32_t> ind;
+        uint32_t length, width, height;
         void addRect(uint32_t i1, uint32_t i2, uint32_t i3, uint32_t i4);
     public:
-        RectangularPrism(uint32_t length, uint32_t width, uint32_t height);
+        RectangularPrism(Canvas* parent, uint32_t length, uint32_t width, uint32_t height);
         ~RectangularPrism();
         double getVolume();
         void init();
