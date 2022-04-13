ed12c24221ffbc82d3a983a8bc59c6f6bb26b40e
ajohnso4 <ajohnso4@stevens.edu>
2022-04-13 13:16:23 -0400

    Rectangular prism bug found and dealt with

--- src/CAD/RectangularPrism.cc
+++ src/CAD/RectangularPrism.cc
@@ -15,10 +15,20 @@ inline void RectangularPrism::addVert(float x, float y, float z) {
   vert.push_back(y);
   vert.push_back(z);
 }
+//Best for GL_TRIANGLES
+// inline void RectangularPrism::addRect(uint32_t i1, uint32_t i2, uint32_t i3, uint32_t i4) {
+//   ind.push_back(i1);  ind.push_back(i2);  ind.push_back(i3);
+//   ind.push_back(i1);  ind.push_back(i3);  ind.push_back(i4);
+// }
+
+//Best for GL_LINES
 inline void RectangularPrism::addRect(uint32_t i1, uint32_t i2, uint32_t i3, uint32_t i4) {
-  ind.push_back(i1);  ind.push_back(i2);  ind.push_back(i3);
-  ind.push_back(i1);  ind.push_back(i3);  ind.push_back(i4);
+  ind.push_back(i1);  ind.push_back(i2);
+  ind.push_back(i2); ind.push_back(i3);
+  ind.push_back(i3); ind.push_back(i4);
+  ind.push_back(i4); ind.push_back(i1);
 }
+
 inline void RectangularPrism::addTri(uint32_t i1, uint32_t i2, uint32_t i3) {
   ind.push_back(i1);  ind.push_back(i2);  ind.push_back(i3);
 }
@@ -26,74 +36,48 @@ inline void RectangularPrism::addTri(uint32_t i1, uint32_t i2, uint32_t i3) {
 void RectangularPrism::init(){
   glGenVertexArrays(1, &vao);
   glBindVertexArray(vao);
+
+  addVert(x, y, z); //0
+  addVert(x + width, y, z); //1
+  addVert(x + width, y + height, z); //2
+  addVert(x, y + height, z); //3
+  addVert(x, y, z + length); //4
+  addVert(x + width, y, z + length); //5
+  addVert(x + width, y + height, z + length); //6
+  addVert(x, y + height, z + length); //7
+
+  //front
+  addRect(0, 1, 2, 3);
+
+  //back
+  addRect(4, 5, 6, 7);
+
+  //sides
+  addRect(1, 5, 6, 2);
+
+  addRect(4, 0, 3, 7);
+
+  //Top
+  addRect(3, 2, 6, 7);
+
+  //Bottom
+  addRect(0, 1, 5, 4);
   
-  //Back Face
-  addVert(x, y, z);
-  addVert(x + width, y, z);
-  addVert(x + width, y + height, z);
-  
-  addVert(x, y, z);
-  addVert(x + width, y + height, z);
-  addVert(x, y + height, z);
-
-  //Front face
-  addVert(x, y, z + length);
-  addVert(x + width, y, z + length);
-  addVert(x + width, y + height, z + length);
-  
-  addVert(x, y, z + length);
-  addVert(x + width, y + height, z + length);
-  addVert(x, y + height, z + length);
-
-
-
-  //Right Face
-  addVert(x + width, y, z);
-  addVert(x + width, y, z + length);
-  addVert(x + width, y + height, z + length);
-
-  addVert(x + width, y, z);
-  addVert(x + width, y + height, z + length);
-  addVert(x + width, y + height, z);
-
-  //Left Face
-  addVert(x, y, z + length);
-  addVert(x, y, z);
-  addVert(x, y + height, z);
-  
-  addVert(x, y, z + length);
-  addVert(x, y + height, z);
-  addVert(x, y + height, z + length);
-
-  //Top Face
-  addVert(x, y + height, z);
-  addVert(x + width, y + height, z);
-  addVert(x + width, y + height, z + length);
-
-  addVert(x, y + height, z);
-  addVert(x + width, y + height, z + length);
-  addVert(x, y + height, z + length);
-
-  //bottom face
-  addVert(x, y, z);
-  addVert(x + width, y, z);
-  addVert(x + width, y, z + length);
-
-  addVert(x, y, z);
-  addVert(x + width, y, z + length);
-  addVert(x, y, z + length);
-
 
 
   glGenBuffers(1, &vbo);
   glBindBuffer(GL_ARRAY_BUFFER, vbo);
   glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vert.size(), &vert[0], GL_STATIC_DRAW);
-  //glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void *) 0);
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 
   glGenBuffers(1,&lbo);
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,lbo);
   glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint)*ind.size(),&ind[0],GL_STATIC_DRAW);
+
+  //use if you want points to be drawn as well
+  // glGenBuffers(1,&pbo);
+  // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,pbo);
+  // glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint)*ind.size(),&ind[0],GL_STATIC_DRAW);
 
 }
 
@@ -102,16 +86,17 @@ void RectangularPrism::render(){
   //shader->setMat4("projection",*(parentCanvas->getProjection()));
 	shader->setVec4("solidColor",style->getFgColor());
   
-  //glDrawArrays(GL_LINE_LOOP, 0, 3);
-  //glDrawArrays(GL_POINTS, 0, 3);
+  //glDrawArrays(GL_LINE_LOOP, 0, vert.size());
 
   glBindVertexArray(vao);
   glEnableVertexAttribArray(0);
   glLineWidth(style->getLineWidth());
-  glDrawArrays(GL_LINES, 0, vert.size());
-  //glDrawArrays(GL_TRIANGLES, 0, vert.size());
-  // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vbo);
-  // glDrawElements(GL_TRIANGLES, ind.size(), GL_UNSIGNED_INT, 0);
+  
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, lbo);
+  glDrawElements(GL_LINES, ind.size(), GL_UNSIGNED_INT, 0);
+  //for points
+  //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, pbo);
+  //glDrawElements(GL_POINTS, ind.size(), GL_UNSIGNED_INT, 0);
 
   glDisableVertexAttribArray(0);
   glBindVertexArray(0);
