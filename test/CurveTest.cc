223764e81da19a49a863aa623322956ebde344d8
kaiqichee <55299653+kaiqichee@users.noreply.github.com>
2022-04-22 16:26:44 -0400

    update CMAKELIST to build helix and extrude along helix
    doesn't look completely 3d because of shading

--- test/CurveTest.cc
+++ test/CurveTest.cc
@@ -1,4 +1,4 @@
-//#include "CAD/Helix.cc"
+#include "CAD/Helix.hh"
 #include "CAD/Triangle.hh"
 #include "CAD/Circle.hh"
 #include "CAD/Line.hh"
@@ -31,26 +31,15 @@ class CurveTest : public GLWin {
   
     //set the camera
     MainCanvas *c = currentTab()->getMainCanvas();
-    Camera* cam = c->setLookAtProjection(20, 0, 50, 0, 0, 0, 0, 1, 0);
+    Camera* cam = c->setLookAtProjection(-80, 0, 30, 0, 0, 0, 0, 1, 0);
     c->setProjection(cam->getViewProjection());
 
-    //rectangle test
-    //StyledMultiShape2D* m = c->addLayer(new StyledMultiShape2D(c, baseGraphStyle));
-    //m->fillRectangle(0,0,100,100,grail::red);
-    
     //helix test
-    // Vec3D start(0,0,0);
-    // Helix* test = new Helix(start, 10, 2, c, baseGraphStyle);
-    // c->addLayer(test);
+    Vec3D start(3,0,0);
+    Helix* htest = new Helix(start, 10, 2, c, baseGraphStyle);
+    c->addLayer(htest);
 
-    
-
-    // c->addLayer(btest);
-    //cout << "new curve test" << endl;
-    
-
-    // Triangle* tritest = new Triangle(c,2,2,2,3,baseGraphStyle);
-    // c->addLayer(tritest);
+  
 
     std::vector<Vec3D> points;
     Vec3D f1(3,0,0);
@@ -63,8 +52,8 @@ class CurveTest : public GLWin {
     points.push_back(f4);
     points.push_back(f3);
 
-    Facet* ftest = new Facet(points,c,baseGraphStyle);
-    c->addLayer(ftest);
+   Facet* ftest = new Facet(points,c,baseGraphStyle);
+    //c->addLayer(ftest);
 
     //bezier test
     Vec3D b1(3,0,0);
@@ -73,15 +62,15 @@ class CurveTest : public GLWin {
     Vec3D b4(12,7,0);
     BezierPath* btest = new BezierPath(b1,b2,b3,b4,c,baseGraphStyle);
 
-//    cout<< "BPOITNS: " << size(btest->points) << endl;
-    c->addLayer(btest);
+  //  c->addLayer(btest);
 
-    double pointSize = size(btest->points);
+    double pointSize = size(htest->points);
     //extrude a triangle along a bezier
     // uncomment line 93 to turn into a square extrusion
+    //change htest to btest to get it to draw on a bezier
      for (double i=0; i<pointSize; i+=1){
       std::vector<Vec3D> points2;
-      Vec3D m = btest->points[i]-btest->points[0];
+      Vec3D m = htest->points[i]-htest->points[0];
 
       Vec3D f5=f1+m;
       Vec3D f6=f2+m;
@@ -90,19 +79,19 @@ class CurveTest : public GLWin {
      
       points2.push_back(f5);
       points2.push_back(f6);
-      //points2.push_back(f8);
+      points2.push_back(f8);
       points2.push_back(f7);
       Facet* ftest2 = new Facet(points2,c,baseGraphStyle);
-      c->addLayer(ftest2);
+     c->addLayer(ftest2);
      }
-    Line* ltest = new Line(f1,f2,c, baseGraphStyle);
-    c->addLayer(ltest);
-    Line* ltest2 = new Line(f2,f3,c, baseGraphStyle);
-    c->addLayer(ltest2);
-    Line* ltest3 = new Line(f3,f1,c, baseGraphStyle);
-    c->addLayer(ltest3);
-  }
-};
+//     Line* ltest = new Line(f1,f2,c, baseGraphStyle);
+//     c->addLayer(ltest);
+//     Line* ltest2 = new Line(f2,f3,c, baseGraphStyle);
+//     c->addLayer(ltest2);
+//     Line* ltest3 = new Line(f3,f1,c, baseGraphStyle);
+//     c->addLayer(ltest3);
+   }
+ };
 
 int main(int argc, char *argv[]) {
   return GLWin::init(new CurveTest(), 1000, 1000);
