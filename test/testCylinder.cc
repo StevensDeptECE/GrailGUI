25df667510c10743bba059547811b825af4d2f57
alikolenovic <akolenov@stevens.edu>
2022-04-30 15:20:53 -0400

    Cylinder with Transform

--- test/testCylinder.cc
+++ test/testCylinder.cc
@@ -2,8 +2,12 @@
 #include "opengl/GrailGUI.hh"
 #include "CAD/Cylinder.hh"
 #include "opengl/util/Camera.hh"
+#include "CAD/Transformation.hh"
+#include <numbers>
+
 using namespace std;
 using namespace grail;
+using namespace std::numbers;
 
 // int main(){
 //     Cylinder cylinder(2, 2, 2);
@@ -26,8 +30,12 @@ class TestCylinder : public GLWin {
         c->setProjection(cam->getViewProjection());
         
         const Style* s = getDefaultStyle();
-        Cylinder* cyl = new Cylinder(c, s, 5, 60, 60, 0, 1, 200);
-        c->addLayer(cyl);
+        // Cylinder* cyl = ;
+        // c->addLayer(cyl);
+        Transformation* rotate = new Transformation(c, s, new Cylinder(c, s, 4, 60, 60, 0, 1, 200));
+        rotate->rotate(pi/3, 0, 2, 0);
+        //rotate->translate(5, 4, 3);
+        c->addLayer(rotate);    
   }
 };
 
