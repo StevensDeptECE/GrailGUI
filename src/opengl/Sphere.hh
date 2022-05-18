50a8c6df1656b230b2e31bc1ae328decd5664731
Alice Huston <shuston@stevens.edu>
2022-05-18 14:14:06 -0400

    Undo textured coord removal

--- src/opengl/Sphere.hh
+++ src/opengl/Sphere.hh
@@ -7,7 +7,8 @@ private:
   std::vector<uint32_t> ind;
   void addRect(uint32_t i1, uint32_t i2, uint32_t i3, uint32_t i4);
   void addTri(uint32_t i1, uint32_t i2, uint32_t i3);
-  void addVert(float x, float y, float z);
+  void addVert(float x, float y, float z); // add just coords
+  void addVert(float x, float y, float z, float u, float v); // add with texture
 public:
   Sphere(uint32_t latRes, uint32_t lonRes);
   ~Sphere();
