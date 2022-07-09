6a45df213292ca1ec823e461cfe8a676dd70f4e9
ajay <athakka5@stevens.edu>
2022-07-09 17:45:51 -0400

    includes drag functionality by deleting and adding image

--- test/2d/testImageMove.cc
+++ test/2d/testImageMove.cc
@@ -1,6 +1,7 @@
 #include "opengl/ButtonWidget.hh"
 #include "opengl/GrailGUI.hh"
 #include "opengl/Image.hh"
+#include "opengl/util/Transformation.hh"
 
 using namespace std;
 using namespace grail;
@@ -14,6 +15,8 @@ class ReactingToInput : public Member {
   void (*p)();  // p points to void functions with empty ()
   float xpos, ypos, width, height;
   Image* piece;
+  Transformation tChess;
+  GLWin* window;
 
   void GetPosition(GLWin* w) {
     xpos = w->mouseX;
@@ -21,12 +24,12 @@ class ReactingToInput : public Member {
   }
 
  public:
-  ReactingToInput(Tab* t, GLWin* w) : Member(t) {
+  ReactingToInput(Tab* t, GLWin* w) : Member(t, 0, .1), window(w) {
     MainCanvas* c = t->getMainCanvas();
 
     GetPosition(w);
-    xpos = 50;
-    ypos = 500;
+    xpos = 100;
+    ypos = 100;
     width = 200;
     height = 200;
 
@@ -44,24 +47,36 @@ class ReactingToInput : public Member {
     // t->bindEvent(Tab::MOUSE0_PRESS, &ReactingToInput::press, this);
 
     t->bindEvent(Tab::MOUSE0_PRESS, [w, this]() { press(w); });
-    t->bindEvent(Tab::MOUSE0_RELEASE, &ReactingToInput::release, this);
+    // t->bindEvent(Tab::MOUSE0_RELEASE, &ReactingToInput::release, this);
+  }
+
+  void update() {
+    if (hasclicked) {
+      c->removeLayer(piece);
+      piece = addNewImage(window->mouseX, window->mouseY);
+      tab->getParentWin()->setUpdate();
+    }
   }
 
   void press(GLWin* w) {
     // turn click into which box you are in
     //  (y - offset) / boxsize = box you are in
-    if (hasclicked == false) {
-      cout << w->mouseX << " " << w->mouseY << " "
-           << "Position Variables"
-           << "\n";
-      hasclicked = true;
-    } else {
-      piece = addNewImage(w->mouseX, w->mouseY);
-      hasclicked = false;
-      tab->getParentWin()->setUpdate();
+    if ((w->mouseX <= xpos + width && w->mouseX > xpos &&
+         w->mouseY <= ypos + height && w->mouseY > ypos) ||
+        hasclicked == true) {
+      if (hasclicked == false) {
+        cout << w->mouseX << " " << w->mouseY << " "
+             << "Position Variables"
+             << "\n";
+        hasclicked = true;
+      } else {
+        GetPosition(w);
+        hasclicked = false;
+        tab->getParentWin()->setUpdate();
+      }
     }
   }
-  void release() {
+  /*void release() {
     if (hasclicked) {
       cout << "RELEASE WORKS!!!!"
            << "\n";
@@ -72,7 +87,7 @@ class ReactingToInput : public Member {
     } else {
       return;
     }
-  }
+  }*/
 
   void drag() {
     // When mouse moves:
